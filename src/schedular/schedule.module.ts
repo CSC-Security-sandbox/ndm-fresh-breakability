80c805e2a82f0ea76f794ec0bd7f0e795c484507
Dinesh Seervi <dinesh.seervi@calfus.com>
2024-11-20 14:41:20 +0530

    added job run to rabbitmq queue

--- src/schedular/schedule.module.ts
+++ src/schedular/schedule.module.ts
@@ -6,13 +6,14 @@ import { JobConfigService } from '../jobconfig/jobconfig.service';
 import { JobConfigEntity } from '../entities/jobconfig.entity';
 import { SchedularController } from './schedule.controller';
 import { JobRunService } from '../jobrun/jobrun.service';
+import { RabbitMqService } from '../events/rabbitmq.service';
 
 @Module({
     imports: [
         ScheduleModule.forRoot(),
         TypeOrmModule.forFeature([JobConfigEntity])
     ],
-    providers: [SchedularService, JobConfigService, JobRunService],
+    providers: [SchedularService, JobConfigService, JobRunService, RabbitMqService],
     controllers: [SchedularController],
 })
 export class SchedularModule {}
--- src/schedular/schedule.service.ts
+++ src/schedular/schedule.service.ts
@@ -3,6 +3,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import { JobConfigService } from '../jobconfig/jobconfig.service';
 import { JobRunService } from '../jobrun/jobrun.service';
+import { RabbitMqService } from '../events/rabbitmq.service';
 
 @Injectable()
 export class SchedularService {
@@ -11,6 +12,7 @@ export class SchedularService {
   constructor (
     private readonly jobConfigService: JobConfigService,
     private readonly jobRunService: JobRunService,
+    private rabbitMqService: RabbitMqService,
   ) {}
 
   async handleCron(): Promise<string> {
@@ -21,7 +23,7 @@ export class SchedularService {
     const jobs = await this.jobConfigService.getJobs({ where: { status: 'Active', schedule_time: Between(windowStart, windowEnd) }});
 
     for (const job of jobs) {
-      await this.jobRunService.createJobRun({
+      const jobRun = await this.jobRunService.createJobRun({
         id: uuid(),
         status: 'RUNNING',
         start_time: currentTime,
@@ -30,6 +32,22 @@ export class SchedularService {
         job_id: job.id
       });
       this.logger.log(`Job run created for job ID: ${job.id} at ${currentTime}`);
+      this.rabbitMqService.publishToExchange({
+        id: uuid(),
+        jobRunId: jobRun.id,
+        taskType: 'SCAN',
+        status: 'PENDING',
+        transactionId: '',
+        fileServerId: job.file_server_id,
+        operations: [{
+          operation: 'SCAN_PATH',
+          request: {
+            pathId: job.path_id,
+            folder: ''
+          },
+          status: 'PENDING'
+        }]
+      });
     }
     return 'success';
   }
