ee56a5bf18b4464c3ce5cca8a11be904124be2b8
AniketCalfus <aniket.darekar@calfus.com>
2024-12-04 14:26:47 +0530

    added back existing rabbitmq serviceQueue

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,24 @@ async function bootstrap() {
   const host: string = configService.get<string>('app.http.host');
   const port: number = configService.get<number>('app.http.port');
 
-  const taskQueueApp = await NestFactory.createMicroservice<MicroserviceOptions>(
+  const serviceQueue = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: configService.get('app.rabbitmq.urls'),
+        queue: configService.get('app.rabbitmq.queue'),
+        noAck: false,
+        queueOptions: {
+          durable: configService.get('app.rabbitmq.durable'),
+          arguments: {
+            'x-queue-type': 'quorum',
+          },
+        },
+      },
+    });
+
+  const taskQueue = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.RMQ,
@@ -37,31 +54,33 @@ async function bootstrap() {
     });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  
+
   app.setGlobalPrefix('api/v1/')
-  
+
   app.useGlobalPipes(new ValidationPipe())
 
   const serverEndpoint = `http://${host}:${port}`;
   const config = new DocumentBuilder()
-  .setTitle('Job service')
-  .setDescription('Job Management')
-  .addServer(serverEndpoint, `Environment`)
-  .setVersion('1.0')
-  .build();
-  
+    .setTitle('Job service')
+    .setDescription('Job Management')
+    .addServer(serverEndpoint, `Environment`)
+    .setVersion('1.0')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('docs', app, document,{
+  SwaggerModule.setup('docs', app, document, {
     jsonDocumentUrl: 'swagger/json',
   });
-  
+
   app.enableShutdownHooks();
   app.set('trust proxy', true);
-  
+
   app.enableCors();
-  
-  await taskQueueApp.listen();
+
+  await serviceQueue.listen();
+  Logger.log('Service Queue Microservice is listening...');
+  await taskQueue.listen();
   Logger.log('Task Queue Microservice is listening...');
   await app.listen(port, '0.0.0.0');
 }
