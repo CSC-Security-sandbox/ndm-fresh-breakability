1a66e76082a8abb841bf23e5d87c84aacf593bd3
kunal77avghade <kunal.avghade@calfus.com>
2025-01-06 15:16:35 +0530

    minor changes

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,41 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Scope } from '@nestjs/common';
 import { Logger } from 'winston';
 
-@Injectable()
+@Injectable({ scope: Scope.TRANSIENT }) 
 export class LoggerService {
+  private parentContext: string;
+
   constructor(private readonly logger: Logger) {}
+
+  setParentContext(context: string) {
+    this.parentContext = context;
+  }
 
   log(message: string, trackId?: string, context?: string) {
     this.logger.log('info', {
-        context: context ?? LoggerService.name,
-        trackId,
-        message,
-    })
+      context: context ?? this.parentContext ?? LoggerService.name,
+      trackId,
+      message,
+    });
   }
 
   error(message: string, trackId?: string, context?: string) {
     this.logger.log('error', {
-        context: context ?? LoggerService.name,
-        trackId,
-        message,
-    })
+      context: context ?? this.parentContext ?? LoggerService.name,
+      trackId,
+      message,
+    });
   }
 
   debug(message: string, trackId?: string, context?: string) {
     this.logger.log('debug', {
-        context: context ?? LoggerService.name,
-        trackId,
-        message,
-    })
+      context: context ?? this.parentContext ?? LoggerService.name,
+      trackId,
+      message,
+    });
   }
 
   warn(message: string, trackId?: string, context?: string) {
     this.logger.log('warn', {
-        context: context ?? LoggerService.name,
-        trackId,
-        message,
-    })
+      context: context ?? this.parentContext ?? LoggerService.name,
+      trackId,
+      message,
+    });
   }
-
-
 }
