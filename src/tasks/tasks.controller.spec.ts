76ce54c4c5d5f93c20fcd2563a3da0e0e5e792e6
kunal77avghade <kunal.avghade@calfus.com>
2025-01-14 12:16:44 +0530

    task listing api

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+import { TaskQueryParamsDto } from './dto/taskpage.dto';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 describe('TasksController', () => {
-  let controller: TasksController;
+  let tasksController: TasksController;
+  let tasksService: TasksService;
+
+  const mockTasksService = {
+    getTaskList: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
+      providers: [{ provide: TasksService, useValue: mockTasksService }],
     }).compile();
 
-    controller = module.get<TasksController>(TasksController);
+    tasksController = module.get<TasksController>(TasksController);
+    tasksService = module.get<TasksService>(TasksService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(tasksController).toBeDefined();
+  });
+
+  describe('getTaskList', () => {
+    const validQueryParams = {
+      page: 1,
+      limit: 10,
+    };
+
+    it('should call taskService.getTaskList with correct parameters', async () => {
+      mockTasksService.getTaskList.mockResolvedValue(['Task1', 'Task2']); 
+      const result = await tasksController.getTaskList(validQueryParams as any);
+      expect(tasksService.getTaskList).toHaveBeenCalledWith(validQueryParams);
+      expect(result).toEqual(['Task1', 'Task2']);
+    });
+
+    it('should throw a BadRequestException for invalid query parameters', async () => {
+      const invalidQueryParams: any = { page: -1, limit: 0 };
+
+      const validationPipe = new ValidationPipe({ transform: false, whitelist: true });
+
+      await expect(validationPipe.transform(invalidQueryParams, {
+        metatype: TaskQueryParamsDto,
+        type: 'query',
+      })).rejects.toThrow(BadRequestException);
+    });
+
+    it('should handle service errors gracefully', async () => {
+      mockTasksService.getTaskList.mockRejectedValue(new Error('Service Error'));
+      await expect(tasksController.getTaskList(validQueryParams as any)).rejects.toThrow('Service Error');
+    });
   });
 });
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,18 +1,130 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { In, Repository } from 'typeorm';
+import { TaskEntity } from 'src/entities/task.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TaskQueryParamsDto } from './dto/taskpage.dto';
+import { Logger } from '@nestjs/common';
+import { TaskStatus } from 'src/constants/enums';
 
 describe('TasksService', () => {
-  let service: TasksService;
+  let tasksService: TasksService;
+  let taskRepo: Repository<TaskEntity>;
+
+  const mockTaskRepo = {
+    find: jest.fn(),
+    count: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        {
+          provide: getRepositoryToken(TaskEntity),
+          useValue: mockTaskRepo,
+        },
+        Logger,
+      ],
     }).compile();
 
-    service = module.get<TasksService>(TasksService);
+    tasksService = module.get<TasksService>(TasksService);
+    taskRepo = module.get<Repository<TaskEntity>>(getRepositoryToken(TaskEntity));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(tasksService).toBeDefined();
+  });
+
+  describe('getTaskList', () => {
+    const taskQueryParams: TaskQueryParamsDto = {
+      page: '1',
+      limit: '10',
+      sort: 'createdAt',
+      order: 'asc',
+      jobRunId: 'testJobRunId',
+    };
+
+    it('should return paginated task list', async () => {
+
+      const mockData = [{ id: 1, name: 'Task 1' }, { id: 2, name: 'Task 2' }];
+      mockTaskRepo.find.mockResolvedValue(mockData);
+      mockTaskRepo.count.mockResolvedValue(2);
+
+      const result = await tasksService.getTaskList(taskQueryParams);
+
+      expect(taskRepo.find).toHaveBeenCalledWith({
+        where: { jobRunId: 'testJobRunId' },
+        order: { createdAt: 'asc' },
+        skip: 0,
+        take: 10,
+      });
+      expect(taskRepo.count).toHaveBeenCalledWith({
+        where: { jobRunId: 'testJobRunId' },
+      });
+      expect(result).toEqual({ data: mockData, total: 2 });
+    });
+
+    it('should return task list without pagination if page and limit are not provided', async () => {
+      const taskQueryParamsWithoutPagination: TaskQueryParamsDto = {
+        sort: 'createdAt',
+        order: 'asc',
+        jobRunId: 'testJobRunId',
+      };
+
+      const mockData = [{ id: 1, name: 'Task 1' }];
+      mockTaskRepo.find.mockResolvedValue(mockData);
+      mockTaskRepo.count.mockResolvedValue(1);
+
+      const result = await tasksService.getTaskList(taskQueryParamsWithoutPagination);
+
+      expect(taskRepo.find).toHaveBeenCalledWith({
+        where: { jobRunId: 'testJobRunId' },
+        order: { createdAt: 'asc' },
+      });
+      expect(taskRepo.count).toHaveBeenCalledWith({
+        where: { jobRunId: 'testJobRunId' },
+      });
+      expect(result).toEqual({ data: mockData, total: 1 });
+    });
+
+    it('should correctly handle filters with In() clause', async () => {
+      const taskQueryWithFilters: TaskQueryParamsDto = {
+        page: '1',
+        limit: '5',
+        sort: 'createdAt',
+        order: 'desc',
+        jobRunId: 'testJobRunId',
+        workerId:['234567-45678-56789'],
+        status: [TaskStatus.Completed, TaskStatus.Errored],
+      };
+
+      mockTaskRepo.find.mockResolvedValue([]);
+      mockTaskRepo.count.mockResolvedValue(0);
+
+      await tasksService.getTaskList(taskQueryWithFilters);
+
+      expect(taskRepo.find).toHaveBeenCalledWith({
+        where: {
+          jobRunId: 'testJobRunId',
+          workerId: In(['234567-45678-56789']),
+          status: In( [TaskStatus.Completed, TaskStatus.Errored]),
+        },
+        order: { createdAt: 'desc' },
+        skip: 0,
+        take: 5,
+      });
+    });
+
+  });
+
+  it('should return an empty result when no workers are found', async () => {
+    const taskQueryWithFilters: TaskQueryParamsDto = { page: '1', limit: '10' };
+    mockTaskRepo.find.mockResolvedValue([]);
+    mockTaskRepo.count.mockResolvedValue(0);
+
+    const result = await tasksService.getTaskList(taskQueryWithFilters);
+
+    expect(result).toEqual({ data: [], total: 0 });
   });
 });
