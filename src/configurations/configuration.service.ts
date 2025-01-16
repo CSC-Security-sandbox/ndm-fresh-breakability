88587a5db9fa25dd2abd9c017435c77cb017ab16
AniketCalfus <aniket.darekar@calfus.com>
2025-01-16 15:39:41 +0530

    relationship name changed from fileServerWorkingDirectoryMapping to workingDirectory in config table

--- src/configurations/configuration.service.ts
+++ src/configurations/configuration.service.ts
@@ -84,7 +84,7 @@ export class ConfigurationService {
                 projectId: true,
                 scannedDate: true,
                 status: true,
-                fileServerWorkingDirectoryMapping: {
+                workingDirectory: {
                     pathName: true,
                     workingDirectory: true
                 },
