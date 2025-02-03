436fbff1a1ba025c19d08be52a4a690726d3d9b9
kunal77avghade <kunal.avghade@calfus.com>
2025-02-03 11:59:04 +0530

    worker auth added

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./auth/AuthKeycloakModule"
 export * from "./auth/JwtAuthGuard"
 export * from "./auth/JwtService"
 export * from './constants/permission'
+export * from "./auth/jwtWorkerAuthGuard"
