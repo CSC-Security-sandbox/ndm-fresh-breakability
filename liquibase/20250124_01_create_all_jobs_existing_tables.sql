de35e8792a872420c80270f5fad0a04b11d17c10
AniketCalfus <aniket.darekar@calfus.com>
2025-02-04 19:33:57 +0530

    added worker table sql in config service

--- liquibase/20250124_01_create_all_jobs_existing_tables.sql
+++ liquibase/20250124_01_create_all_jobs_existing_tables.sql
@@ -102,21 +102,6 @@ CREATE INDEX idx_file_path_task ON operations USING btree (f_path, task_id);
 CREATE INDEX idx_operation_run_status ON operations USING btree (job_run_id, status);
 CREATE INDEX idx_operation_type ON operations USING btree (operation_type);
 
-CREATE TABLE IF NOT EXISTS worker (
-	created_at timestamp DEFAULT now() NOT NULL,
-	updated_at timestamp DEFAULT now() NULL,
-	created_by varchar NULL,
-	updated_by varchar NULL,
-	id uuid NOT NULL,
-	project_id uuid NOT NULL,
-	client_id varchar(255) NOT NULL,
-	worker_name varchar(255) NOT NULL,
-	ip_address varchar(255) NOT NULL,
-	status varchar NOT NULL,
-	CONSTRAINT "PK_dc8175fa0e34ce7a39e4ec73b94" PRIMARY KEY (id),
-	CONSTRAINT "FK_787ef3391e00fbbd3c127e0f3a2" FOREIGN KEY (project_id) REFERENCES project(id)
-);
-
 CREATE TABLE IF NOT EXISTS request_track (
 	created_at timestamp DEFAULT now() NOT NULL,
 	updated_at timestamp DEFAULT now() NULL,
