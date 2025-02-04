b221af714ad87775a79ae1b02a74fefbad4fe6dd
AniketCalfus <aniket.darekar@calfus.com>
2025-02-04 17:26:38 +0530

    Changed the order of the scripts execution

--- liquibase/20250124_01_create_all_jobs_existing_tables.sql
+++ liquibase/20250124_01_create_all_jobs_existing_tables.sql
@@ -32,6 +32,22 @@ CREATE TABLE IF NOT EXISTS jobidmapping (
 	CONSTRAINT "PK_49659c592bf37c883fca075529c" PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS jobrun (
+	created_at timestamp DEFAULT now() NOT NULL,
+	updated_at timestamp DEFAULT now() NULL,
+	created_by varchar NULL,
+	updated_by varchar NULL,
+	id uuid DEFAULT uuid_generate_v4() NOT NULL,
+	status varchar NOT NULL,
+	start_time timestamp NOT NULL,
+	end_time timestamp NULL,
+	iteration_number int4 NOT NULL,
+	job_config_id uuid NOT NULL,
+	job_options_id uuid NULL,
+	CONSTRAINT "PK_39c91b190948f08d1d392f404e6" PRIMARY KEY (id),
+	CONSTRAINT "FK_90c86a3ffe6d7381cdc8dcab5b1" FOREIGN KEY (job_config_id) REFERENCES jobconfig(id) ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS job_options (
 	created_at timestamp DEFAULT now() NOT NULL,
 	updated_at timestamp DEFAULT now() NULL,
@@ -48,21 +64,20 @@ CREATE TABLE IF NOT EXISTS job_options (
 	CONSTRAINT "REL_5b29fd4de00a0a910f667243c7" UNIQUE (job_run_id)
 );
 
-CREATE TABLE IF NOT EXISTS jobrun (
-	created_at timestamp DEFAULT now() NOT NULL,
-	updated_at timestamp DEFAULT now() NULL,
-	created_by varchar NULL,
-	updated_by varchar NULL,
+CREATE TABLE IF NOT EXISTS tasks (
 	id uuid DEFAULT uuid_generate_v4() NOT NULL,
+	job_run_id uuid NOT NULL,
 	status varchar NOT NULL,
-	start_time timestamp NOT NULL,
-	end_time timestamp NULL,
-	iteration_number int4 NOT NULL,
-	job_config_id uuid NOT NULL,
-	job_options_id uuid NULL,
-	CONSTRAINT "PK_39c91b190948f08d1d392f404e6" PRIMARY KEY (id),
-	CONSTRAINT "FK_90c86a3ffe6d7381cdc8dcab5b1" FOREIGN KEY (job_config_id) REFERENCES jobconfig(id) ON DELETE CASCADE
+	task_type varchar NULL,
+	worker_id uuid NULL,
+	created_at timestamp DEFAULT now() NOT NULL,
+	updated_at timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "PK_8d12ff38fcc62aaba2cab748772" PRIMARY KEY (id),
+	CONSTRAINT "FK_e1d40541e2ee780839fb3f95c4a" FOREIGN KEY (job_run_id) REFERENCES jobrun(id) ON DELETE CASCADE
 );
+CREATE INDEX idx_job_run_id ON tasks USING btree (job_run_id);
+CREATE INDEX idx_job_run_status ON tasks USING btree (job_run_id, status);
+CREATE INDEX idx_task_type ON tasks USING btree (task_type);
 
 CREATE TABLE IF NOT EXISTS operations (
 	created_at timestamp DEFAULT now() NOT NULL,
@@ -87,6 +102,21 @@ CREATE INDEX idx_file_path_task ON operations USING btree (f_path, task_id);
 CREATE INDEX idx_operation_run_status ON operations USING btree (job_run_id, status);
 CREATE INDEX idx_operation_type ON operations USING btree (operation_type);
 
+CREATE TABLE IF NOT EXISTS worker (
+	created_at timestamp DEFAULT now() NOT NULL,
+	updated_at timestamp DEFAULT now() NULL,
+	created_by varchar NULL,
+	updated_by varchar NULL,
+	id uuid NOT NULL,
+	project_id uuid NOT NULL,
+	client_id varchar(255) NOT NULL,
+	worker_name varchar(255) NOT NULL,
+	ip_address varchar(255) NOT NULL,
+	status varchar NOT NULL,
+	CONSTRAINT "PK_dc8175fa0e34ce7a39e4ec73b94" PRIMARY KEY (id),
+	CONSTRAINT "FK_787ef3391e00fbbd3c127e0f3a2" FOREIGN KEY (project_id) REFERENCES project(id)
+);
+
 CREATE TABLE IF NOT EXISTS request_track (
 	created_at timestamp DEFAULT now() NOT NULL,
 	updated_at timestamp DEFAULT now() NULL,
@@ -104,21 +134,6 @@ CREATE TABLE IF NOT EXISTS request_track (
 	CONSTRAINT "FK_8363d1ef7f84cc88ec34d6dfec7" FOREIGN KEY (worker_id) REFERENCES worker(id)
 );
 
-CREATE TABLE IF NOT EXISTS tasks (
-	id uuid DEFAULT uuid_generate_v4() NOT NULL,
-	job_run_id uuid NOT NULL,
-	status varchar NOT NULL,
-	task_type varchar NULL,
-	worker_id uuid NULL,
-	created_at timestamp DEFAULT now() NOT NULL,
-	updated_at timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "PK_8d12ff38fcc62aaba2cab748772" PRIMARY KEY (id),
-	CONSTRAINT "FK_e1d40541e2ee780839fb3f95c4a" FOREIGN KEY (job_run_id) REFERENCES jobrun(id) ON DELETE CASCADE
-);
-CREATE INDEX idx_job_run_id ON tasks USING btree (job_run_id);
-CREATE INDEX idx_job_run_status ON tasks USING btree (job_run_id, status);
-CREATE INDEX idx_task_type ON tasks USING btree (task_type);
-
 CREATE TABLE IF NOT EXISTS volume (
 	created_at timestamp DEFAULT now() NOT NULL,
 	updated_at timestamp DEFAULT now() NULL,
@@ -132,21 +147,6 @@ CREATE TABLE IF NOT EXISTS volume (
 	is_baseline_migration_done bool DEFAULT false NULL,
 	CONSTRAINT "PK_666025cd0c36727216bb7f2a680" PRIMARY KEY (id),
 	CONSTRAINT "FK_556b3e916c55734c6f7424c9280" FOREIGN KEY (file_server_id) REFERENCES file_server(id) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS worker (
-	created_at timestamp DEFAULT now() NOT NULL,
-	updated_at timestamp DEFAULT now() NULL,
-	created_by varchar NULL,
-	updated_by varchar NULL,
-	id uuid NOT NULL,
-	project_id uuid NOT NULL,
-	client_id varchar(255) NOT NULL,
-	worker_name varchar(255) NOT NULL,
-	ip_address varchar(255) NOT NULL,
-	status varchar NOT NULL,
-	CONSTRAINT "PK_dc8175fa0e34ce7a39e4ec73b94" PRIMARY KEY (id),
-	CONSTRAINT "FK_787ef3391e00fbbd3c127e0f3a2" FOREIGN KEY (project_id) REFERENCES project(id)
 );
 
 CREATE TABLE IF NOT EXISTS worker_jobrun_mapping (
