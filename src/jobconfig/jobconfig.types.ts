d77e6f0d2788f8ef9b9ef48bf5dcb69e8b6d815f
Dinesh Seervi <dinesh.seervi@calfus.com>
2025-02-21 16:22:28 +0530

    app start fix

--- src/jobconfig/jobconfig.types.ts
+++ src/jobconfig/jobconfig.types.ts
@@ -22,7 +22,7 @@ export interface JobConfigBulkCutoverRes {
   status: JobStatus;
   firstRunAt: Date;
   sourcePathId: string;
-  targetPathId: string[];
+  targetPathId: string;
 }
 export type FlattenedCutoverConfig = {
     sourcePathId: string;
