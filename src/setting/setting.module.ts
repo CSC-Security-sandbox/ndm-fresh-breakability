4e62fd6371d5979813ff2e3c47efeb5d28fda950
shivamsaxena-calfus <shivam.saxena@calfus.com>
2025-03-05 14:34:41 +0530

    fix jwt dependency

--- src/setting/setting.module.ts
+++ src/setting/setting.module.ts
@@ -3,12 +3,14 @@ import { SettingService } from './setting.service';
 import { SettingController } from './setting.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GlobalSettings } from 'src/entities/global-setting.entity';
+import {
+  AuthKeycloakModule,
+  JwtService,
+} from '@netapp-cloud-datamigrate/auth-lib';
 
 @Module({
-  imports: [  TypeOrmModule.forFeature([
-    GlobalSettings,
-  ]),],
+  imports: [TypeOrmModule.forFeature([GlobalSettings, AuthKeycloakModule])],
   controllers: [SettingController],
-  providers: [SettingService],
+  providers: [SettingService, JwtService],
 })
 export class SettingModule {}
