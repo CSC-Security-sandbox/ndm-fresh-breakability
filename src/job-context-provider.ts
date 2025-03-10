c582e142499f196e5a53c4271d5c7c4ee356b006
verma <aditya.verma2@netapp.com>
2025-03-10 19:07:36 +0530

    Fixed speed-test-config and context

--- src/job-context-provider.ts
+++ src/job-context-provider.ts
@@ -1,9 +1,6 @@
 import { JobContext } from './types/job-context';
 import { JobConfig } from './types/job-config';
 import { JobState } from './types/job-state';
-import { SpeedTestJobContext } from './types/speed-test-job-context';
-import { SpeedTestJobConfig } from './types/speed-test-job-config';
-
 
 export interface JobContextProvider {
   buildContext(
@@ -13,14 +10,6 @@ export interface JobContextProvider {
     JobState: JobState,
   ): Promise<JobContext>;
   
-  buildContext(
-    jobRunId: string,
-    jobConfig: SpeedTestJobConfig,
-    jobStatus: string,
-    JobState: JobState,
-  ): Promise<SpeedTestJobContext>;
-
-  getJobContext(jobRunId: string): Promise<SpeedTestJobContext | null>;
 
   getJobContext(jobRunId: string): Promise<JobContext | null>;
 }
--- src/types/speed-test-job-config.spec.ts
+++ src/types/speed-test-job-config.spec.ts
@@ -6,9 +6,9 @@ describe('SpeedTestJobConfig Class', () => {
   it('should create and serialize SpeedTestJobConfig', () => {
     const sourceFileServer = new FileServerDetails('host', [ new NFS('root') ], 'pathId', 'path', 'username', 'password', 'workingDirectory');
     const fs2 = new FileServerDetails('', [ new NFS('') ], 'pathId', 'path', 'username', 'password', 'workingDirectory');
-    const speedTestJobConfig = new SpeedTestJobConfig('job1', 'type1', sourceFileServer, '/source');
+    const speedTestJobConfig = new SpeedTestJobConfig('job1', 'type1');
     const serialized = speedTestJobConfig.serialize();
-    const newSpeedTestJobConfig = new SpeedTestJobConfig('', '', fs2, '');
+    const newSpeedTestJobConfig = new SpeedTestJobConfig('', '');
     newSpeedTestJobConfig.deserialize(serialized);
     expect(newSpeedTestJobConfig.jobId).toBe('job1');
   });
