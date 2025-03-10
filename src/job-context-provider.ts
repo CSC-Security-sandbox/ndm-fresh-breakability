907f26a23227d680338fa93b5c44b4174b22cd2b
verma <aditya.verma2@netapp.com>
2025-03-10 18:07:42 +0530

    Fixed speed-test-config and context

--- src/job-context-provider.ts
+++ src/job-context-provider.ts
@@ -1,6 +1,9 @@
 import { JobContext } from './types/job-context';
 import { JobConfig } from './types/job-config';
 import { JobState } from './types/job-state';
+import { SpeedTestJobContext } from './types/speed-test-job-context';
+import { SpeedTestJobConfig } from './types/speed-test-job-config';
+
 
 export interface JobContextProvider {
   buildContext(
@@ -9,6 +12,15 @@ export interface JobContextProvider {
     jobStatus: string,
     JobState: JobState,
   ): Promise<JobContext>;
+  
+  buildContext(
+    jobRunId: string,
+    jobConfig: SpeedTestJobConfig,
+    jobStatus: string,
+    JobState: JobState,
+  ): Promise<SpeedTestJobContext>;
+
+  getJobContext(jobRunId: string): Promise<SpeedTestJobContext | null>;
 
   getJobContext(jobRunId: string): Promise<JobContext | null>;
 }
