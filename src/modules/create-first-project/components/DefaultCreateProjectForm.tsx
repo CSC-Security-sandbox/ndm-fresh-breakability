af54ebd08a61e3cb56f1d45c874c1c703b59474a
GRamyaCal <ramyag1@netapp.com>
2025-03-10 15:19:55 +0530

    Added create first project related components

---
+++ src/modules/create-first-project/components/DefaultCreateProjectForm.tsx
@@ -0,0 +1,68 @@
+import { Box } from "@components/container/index";
+import AppFooter from "@/components/layout/app-footer/AppFooter";
+import withCreateProject from "@/hoc/withCreateProject";
+import { setProject } from "@store/reducer/appSlice";
+import {
+  Button,
+  Card,
+  FormFieldInputNew,
+  Layout,
+  TabHeader,
+  FormFieldTextArea,
+} from "@netapp/bxp-design-system-react";
+import {
+  BlueXpFormType,
+} from "@/types/app.type";
+import { useDispatch } from "react-redux";
+
+interface DefaultCreateProjectFormType {
+  createProjectForm: BlueXpFormType<DefaultCreateProjectFormType>;
+  handleCreateProject: () => void;
+}
+
+const DefaultCreateProjectForm = ({
+  createProjectForm,
+  handleCreateProject,
+}: DefaultCreateProjectFormType) => {
+  const dispatch = useDispatch();
+
+  const handleProjectCreation = async () => {
+    const response : any = await handleCreateProject();
+    dispatch(setProject(response?.id || ""));
+  }
+
+  const FOOTER_CONTENT = (
+    <Button onClick={handleProjectCreation} disabled={!createProjectForm.isValid}>
+      Create
+    </Button>
+  );
+  return (
+    <Box className="h-screen">
+      <TabHeader label="Project Details" />
+      <Layout.Content style={{ padding: 40 }}>
+        <Card className="p-6 m-6">
+          <Box className="w-4/6">
+            <FormFieldInputNew
+              form={createProjectForm}
+              name="project_name"
+              placeholder="Project Name"
+              label="Project Name"
+            />
+            <FormFieldTextArea
+              isOptional
+              form={createProjectForm}
+              name="project_description"
+              placeholder="Project Description"
+              label="Project Description"
+              charCount={true}
+              maxChars={500}
+            />
+          </Box>
+        </Card>
+      </Layout.Content>
+      <AppFooter footerContent={FOOTER_CONTENT} />
+    </Box>
+  );
+};
+
+export default withCreateProject(DefaultCreateProjectForm);
