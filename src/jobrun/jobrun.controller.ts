4c3ac1c81666b8c26db40058b583dc7012f4f964
Avadoot Narvekar <avadoot.narvekar@netapp.com>
2025-03-12 21:41:17 +0530

    URI issued fixed and fixed issue when retry count is null

--- src/jobrun/jobrun.controller.ts
+++ src/jobrun/jobrun.controller.ts
@@ -1,100 +1,154 @@
-import { Body, Controller, Get, Logger, Param, Patch, Post, Put, Query, ValidationPipe } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
-import { ApiBadRequestResponse, ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  ValidationPipe,
+} from "@nestjs/common";
+import { Cron, CronExpression } from "@nestjs/schedule";
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from "@nestjs/swagger";
 
-import { JobRunDetailsDTO } from './dto/jobrun.dto';
-import {  ApprovalRequestDTO, JobRunActionsReq } from './dto/jobrunactions.dto';
-import { JobRunPageDto, JobRunPageResponseDto } from './dto/jobrunpage.dto';
-import { JobRunService } from './jobrun.service';
-import { AdHocRunDTO } from './dto/adhockjobrun.dto';
-import { CutOverStatus, JobRunStatus } from 'src/constants/enums';
-import { JobRunInitService } from './jobrun.init.service';
-import { JobErrorQueryDto} from './dto/jobRunErrors.dto';
+import { JobRunDetailsDTO } from "./dto/jobrun.dto";
+import { ApprovalRequestDTO, JobRunActionsReq } from "./dto/jobrunactions.dto";
+import { JobRunPageDto, JobRunPageResponseDto } from "./dto/jobrunpage.dto";
+import { JobRunService } from "./jobrun.service";
+import { AdHocRunDTO } from "./dto/adhockjobrun.dto";
+import { CutOverStatus, JobRunStatus } from "src/constants/enums";
+import { JobRunInitService } from "./jobrun.init.service";
+import { JobErrorQueryDto } from "./dto/jobRunErrors.dto";
 
-@ApiTags('jobs run')
-@Controller('job-run')
+@ApiTags("jobs run")
+@Controller("job-run")
 export class JobRunController {
   private readonly logger = new Logger(JobRunController.name);
   constructor(
     private readonly jobRunService: JobRunService,
     private readonly jobRunInitService: JobRunInitService
-    ) {}
+  ) {}
 
   // remove the schedule cron job
   @Cron(CronExpression.EVERY_10_SECONDS)
-  async handleCron(){
-    await this.jobRunInitService.scheduleAJob()
+  async handleCron() {
+    await this.jobRunInitService.scheduleAJob();
   }
 
-
-  @ApiOperation({ summary: 'Get a paginated list of  Job Run',  description: 'Returns a list of  Job Run based on the provided pagination parameters.'})
-  @ApiOkResponse({ description: 'The list of Job Run has been retrieved successfully.',  type: JobRunPageResponseDto})
-  @ApiBadRequestResponse({
-      description: 'Invalid pagination parameters.'
+  @ApiOperation({
+    summary: "Get a paginated list of  Job Run",
+    description:
+      "Returns a list of  Job Run based on the provided pagination parameters.",
   })
-  @Get('/')
-  async getJobRuns(@Query(new ValidationPipe({ transform: false, whitelist: true }))  jobRunPageDto: JobRunPageDto) {
-      return await this.jobRunService.getJobAllRuns(jobRunPageDto);
+  @ApiOkResponse({
+    description: "The list of Job Run has been retrieved successfully.",
+    type: JobRunPageResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: "Invalid pagination parameters.",
+  })
+  @Get("/")
+  async getJobRuns(
+    @Query(new ValidationPipe({ transform: false, whitelist: true }))
+    jobRunPageDto: JobRunPageDto
+  ) {
+    return await this.jobRunService.getJobAllRuns(jobRunPageDto);
   }
 
-  @ApiOperation({ summary: 'Get job run by ID' })
-  @ApiResponse({ status: 200, description: 'Returns a job run by its ID.' })
-  @ApiResponse({ status: 404, description: 'Job run not found.' })
-  @Get(':id')
-  async getJobById(@Param('id') id: string): Promise<JobRunDetailsDTO> {
+  @ApiOperation({ summary: "Get Job Run Errors" })
+  @ApiResponse({
+    status: 200,
+    description: "The job run errors retrieved successfully .",
+  })
+  @Get("/errors")
+  async getJobRunErrors(
+    @Query(new ValidationPipe({ transform: false, whitelist: true }))
+    jobErrorQuery: JobErrorQueryDto
+  ) {
+    return this.jobRunService.getJobRunErrors(jobErrorQuery);
+  }
+
+  @ApiOperation({ summary: "Get job run by ID" })
+  @ApiResponse({ status: 200, description: "Returns a job run by its ID." })
+  @ApiResponse({ status: 404, description: "Job run not found." })
+  @Get(":id")
+  async getJobById(@Param("id") id: string): Promise<JobRunDetailsDTO> {
     return await this.jobRunService.getJobRun(id);
   }
 
-
-  @ApiOperation({ summary: 'Job Run Actions PAUSE | RESUME | STOP' })
-  @ApiResponse({ status: 200, description: 'The job run action completed successfully .' })
-  @Put('/action')
+  @ApiOperation({ summary: "Job Run Actions PAUSE | RESUME | STOP" })
+  @ApiResponse({
+    status: 200,
+    description: "The job run action completed successfully .",
+  })
+  @Put("/action")
   async actions(@Body() jobRunActions: JobRunActionsReq) {
-    return this.jobRunService.actions(jobRunActions)
+    return this.jobRunService.actions(jobRunActions);
   }
 
-  @ApiOperation({ summary: 'Approve cutover by jon run ID' })
-  @ApiResponse({ status: 200, description: 'The cutover job approved successfully.' })
-  @Put('/cutover/approve')
+  @ApiOperation({ summary: "Approve cutover by jon run ID" })
+  @ApiResponse({
+    status: 200,
+    description: "The cutover job approved successfully.",
+  })
+  @Put("/cutover/approve")
   async cutoverApprove(@Body() approval: ApprovalRequestDTO) {
-    this.logger.log(approval)
+    this.logger.log(approval);
     return this.jobRunService.approveCutoverRequest(approval);
   }
 
-  @ApiOperation({ summary: 'Creates excesive job run based on job config' })
-  @ApiResponse({ status: 200, description: 'The job run created completed successfully .' })
-  @Post('/ad-hoc')
+  @ApiOperation({ summary: "Creates excesive job run based on job config" })
+  @ApiResponse({
+    status: 200,
+    description: "The job run created completed successfully .",
+  })
+  @Post("/ad-hoc")
   async adhocRun(@Body() adhocRun: AdHocRunDTO) {
-    return this.jobRunService.addHocRun(adhocRun.jobConfigId)
+    return this.jobRunService.addHocRun(adhocRun.jobConfigId);
   }
 
-  @ApiOperation({ summary: 'Update Job Run Status' })
-  @ApiResponse({ status: 200, description: 'The job run status updated successfully .' })
-  @Patch('/:jobRunId/:status')
-  async updateJobRunStatus(@Param('jobRunId') jobRunId: string, @Param('status') status: JobRunStatus) {
-    console.log('updatingStatus' + 'jobRunId', jobRunId, 'status', status);
+  @ApiOperation({ summary: "Update Job Run Status" })
+  @ApiResponse({
+    status: 200,
+    description: "The job run status updated successfully .",
+  })
+  @Patch("/:jobRunId/:status")
+  async updateJobRunStatus(
+    @Param("jobRunId") jobRunId: string,
+    @Param("status") status: JobRunStatus
+  ) {
+    console.log("updatingStatus" + "jobRunId", jobRunId, "status", status);
     return await this.jobRunService.updateJobRunStatus(jobRunId, status);
   }
 
-  @ApiOperation({ summary: 'Approve cutover by jon run ID' })
-  @ApiResponse({ status: 200, description: 'The cutover job approved successfully.' })
-  @Put('/cutover/:jobRunId/:status')
-  async cutoverApproval(@Param('jobRunId') jobRunId: string, @Param('status') status: CutOverStatus) {
-
+  @ApiOperation({ summary: "Approve cutover by jon run ID" })
+  @ApiResponse({
+    status: 200,
+    description: "The cutover job approved successfully.",
+  })
+  @Put("/cutover/:jobRunId/:status")
+  async cutoverApproval(
+    @Param("jobRunId") jobRunId: string,
+    @Param("status") status: CutOverStatus
+  ) {
     return this.jobRunService.cutOverApproval(jobRunId, status);
   }
 
-  @ApiOperation({ summary: 'Get Job Run Errors' })
-  @ApiResponse({ status: 200, description: 'The job run errors retrieved successfully .' })
-  @Get('/:jobRunId/errors')
-  async getJobRunErrors(@Query(new ValidationPipe({ transform: false, whitelist: true })) jobErrorQuery: JobErrorQueryDto) {
-    return  this.jobRunService.getJobRunErrors(jobErrorQuery);
-  }
-
-  @ApiOperation({ summary: 'Get Job Run Error Overview' })
-  @ApiResponse({ status: 200, description: 'The job run error overview retrieved successfully .' })
-  @Get('/:jobRunId/errors/overview')
-  async getErrorOverview(@Param('jobRunId') jobRunId: string){
+  @ApiOperation({ summary: "Get Job Run Error Overview" })
+  @ApiResponse({
+    status: 200,
+    description: "The job run error overview retrieved successfully .",
+  })
+  @Get("/:jobRunId/errors/overview")
+  async getErrorOverview(@Param("jobRunId") jobRunId: string) {
     return this.jobRunService.getErrorOverview(jobRunId);
   }
 }
