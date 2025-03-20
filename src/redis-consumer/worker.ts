f6c44e19e52141ad991e5aca0d9c195c6022cd8f
Bhupender Keswani <bhupender.keswani@netapp.com>
2025-03-20 18:38:43 +0530

    worker removed

--- src/redis-consumer/worker.ts
+++
@@ -1,8 +0,0 @@
-const { parentPort, workerData } = require('worker_threads');
-
-function processData(jobRunId) {
-    parentPort.postMessage(`Processing job: ${jobRunId}`);
-    setTimeout(() => processData(jobRunId), 5000);
-}
-
-processData(workerData.jobRunId);
