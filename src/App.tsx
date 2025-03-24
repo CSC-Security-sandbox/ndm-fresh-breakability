5c40ae6446648c2077e46ea94f180aaaba1c0913
Rutuja Deshmukh <rutuja.deshmukh@calfus.com>
2025-03-24 17:08:28 +0530

    Show component used

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,35 @@ import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "@auth/protected-route/ProtectedRoute";
 import { routeConfig } from "@routes/RouteConfig";
 import { Box } from "@components/container/index";
+import { Show } from "@components/show/Show";
+
+const renderRoutes = (routes) =>
+  routes.map((route) => (
+    <Route
+      key={route.path}
+      path={route.path}
+      element={
+        <Show>
+          <Show.When isTrue={route.protected}>
+            <ProtectedRoute
+              requiredPermission={route.requiredPermission}
+              redirectTo="/no-access"
+            >
+              {route.element}
+            </ProtectedRoute>
+          </Show.When>
+          <Show.Else>{route.element}</Show.Else>
+        </Show>
+      }
+    />
+  ));
 
 const App = () => {
   return (
     <Suspense fallback={<Box>Loading...</Box>}>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {routeConfig.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={
-                route.protected ? (
-                  <ProtectedRoute
-                    requiredPermission={route.requiredPermission}
-                    redirectTo="/no-access"
-                  >
-                    {route.element}
-                  </ProtectedRoute>
-                ) : (
-                  route.element
-                )
-              }
-            />
-          ))}
+          {renderRoutes(routeConfig)}
         </Route>
       </Routes>
     </Suspense>
