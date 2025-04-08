e1905cd6aa5389509429d156b8838e1bcc6173bd
abhishek-buragad <aburagad@netapp.com>
2025-04-08 19:38:11 +0530

    [NDM-1061]update status based on health metrics (#158)

--- src/workers/workers.service.ts
+++ src/workers/workers.service.ts
@@ -4,15 +4,44 @@ import { FindManyOptions, Repository } from "typeorm";
 
 import { WorkerEntity } from "src/entities/worker.entity";
 import { WorkersStatusPageDto } from "./dto/workers.page.dto";
+import { WorkerStatus } from "src/constants/enums";
+import { HealthStatus } from "./worker.types";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class WorkersService {
   private logger: Logger = new Logger(WorkersService.name);
-
   constructor(
     @InjectRepository(WorkerEntity)
-    private readonly WorkerEntity: Repository<WorkerEntity>
+    private readonly WorkerEntity: Repository<WorkerEntity>,
+    private readonly configService: ConfigService,
   ) {}
+
+  updateWorkerStatus(workers: WorkerEntity[]) {
+    const timeout = this.configService.get(
+      "app.worker.healthCheckStatusTimout",
+    );
+    console.log("timeout config", timeout);
+    return workers.map((worker) => {
+      if (!worker.stats || !worker.stats?.healthStatus) {
+        worker.status = WorkerStatus.Offline;
+      } else {
+        const updatedAt = new Date(worker.stats.updatedAt);
+        const currentTime = new Date();
+        const timeDiff = Math.abs(currentTime.getTime() - updatedAt.getTime());
+        const diffInSeconds = Math.floor(timeDiff / 1000);
+        if (
+          diffInSeconds >= timeout ||
+          worker.stats.healthStatus !== HealthStatus.Healthy
+        ) {
+          worker.status = WorkerStatus.Offline;
+        } else {
+          worker.status = WorkerStatus.Online;
+        }
+      }
+      return worker;
+    });
+  }
 
   async findAllWorkers(workerStatusPageDto: WorkersStatusPageDto) {
     const {
@@ -25,7 +54,7 @@ export class WorkersService {
     } = workerStatusPageDto;
 
     const whereCondition: any = { ...filter };
-    const updateFilter:any = { ...filter };
+    const updateFilter: any = { ...filter };
     if (jobRunId) {
       whereCondition.jobRunMap = { jobRunId };
       updateFilter.jobRunMap = { jobRunId };
@@ -34,6 +63,7 @@ export class WorkersService {
     const findOptions: FindManyOptions<WorkerEntity> = {
       where: whereCondition,
       order: { [sort]: order },
+      relations: ["stats"],
     };
 
     let data = [],
@@ -47,6 +77,7 @@ export class WorkersService {
       data = await this.WorkerEntity.find(findOptions);
       total = await this.WorkerEntity.count({ where: updateFilter });
     }
-    return { data, total };
+    const workerWithStatusUpdated = this.updateWorkerStatus(data);
+    return { data: workerWithStatusUpdated, total };
   }
 }
