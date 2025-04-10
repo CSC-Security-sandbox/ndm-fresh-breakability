da8452cd38f822da0d418fedcb03ce6b4ff9047b
Rutuja Deshmukh <rutuja.deshmukh@calfus.com>
2025-04-10 15:48:20 +0530

    Common function to format bytes

--- src/utils/job-utils.ts
+++ src/utils/job-utils.ts
@@ -3,3 +3,15 @@ export abstract class JobUtils {
     return `${jobRunId}-${key}`;
   }
 }
+
+export function formatBytes(bytes: number, decimals = 2): string {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(decimals))} ${
+    sizes[i] || sizes[0]
+  }`;
+}
