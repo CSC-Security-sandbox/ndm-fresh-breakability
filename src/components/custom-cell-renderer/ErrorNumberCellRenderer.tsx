7a4c64a7e4d1dd591f0b83d2c8302bb125dc6c42
GRamyaCal <ramyag1@netapp.com>
2025-04-21 20:11:13 +0530

    Summing up the error count and added to the Error column of Run History table in job details page

--- src/components/custom-cell-renderer/ErrorNumberCellRenderer.tsx
+++ src/components/custom-cell-renderer/ErrorNumberCellRenderer.tsx
@@ -1,8 +1,15 @@
 import { Span } from "@netapp/bxp-design-system-react";
 
-const ErrorNumberCellRenderer = ({ value }: { value: number }) => {
-  const props = value > 0 ? { color: "error" } : {};
-  return <Span {...props}>{value || "-"}</Span>;
+interface errors {
+  errortype: string;
+  count: string;
+}
+
+const ErrorNumberCellRenderer = ({ value }: { value: errors[] }) => {
+  const props = value.length > 0 ? { color: "error" } : {};
+  const errorCount = value.length > 0 ? value.reduce((acc, error) => acc + Number(error.count), 0) : "-";
+
+  return <Span {...props}>{errorCount}</Span>;
 };
 
 export default ErrorNumberCellRenderer;
