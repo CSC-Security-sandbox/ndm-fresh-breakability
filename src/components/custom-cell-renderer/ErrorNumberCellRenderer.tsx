5185d1c09a213d2ad454ec759bffb0526b7be9e5
GRamyaCal <ramyag1@netapp.com>
2025-04-22 16:48:18 +0530

    added inline type check instead of seperate interface

--- src/components/custom-cell-renderer/ErrorNumberCellRenderer.tsx
+++ src/components/custom-cell-renderer/ErrorNumberCellRenderer.tsx
@@ -1,13 +1,8 @@
 import { Span } from "@netapp/bxp-design-system-react";
 
-interface errors {
-  errortype: string;
-  count: string;
-}
-
-const ErrorNumberCellRenderer = ({ value }: { value: errors[] }) => {
+const ErrorNumberCellRenderer = ({ value }: { value: { errortype: string, count: string}[] }) => {
   const props = value.length > 0 ? { color: "error" } : {};
-  const errorCount = value.length > 0 ? value.reduce((acc, error) => acc + Number(error.count), 0) : "-";
+  const errorCount = value.length > 0 ? value.reduce((totalJobRunErrors, error) => totalJobRunErrors + Number(error.count), 0) : "-";
 
   return <Span {...props}>{errorCount}</Span>;
 };
