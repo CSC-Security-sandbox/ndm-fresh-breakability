b00b2f43125c3cd9c38f2c83115d780e38a011e3
GRamyaCal <ramyag1@netapp.com>
2025-04-23 15:06:28 +0530

    made the code more readable

--- src/components/custom-cell-renderer/ErrorNumberCellRenderer.tsx
+++ src/components/custom-cell-renderer/ErrorNumberCellRenderer.tsx
@@ -1,8 +1,18 @@
+import React, { useMemo } from "react";
 import { Span } from "@netapp/bxp-design-system-react";
+import { ErrorNumberCellRendererProps } from "@/types/app.type";
 
-const ErrorNumberCellRenderer = ({ value }: { value: { errortype: string, count: string}[] }) => {
+const ErrorNumberCellRenderer = ({ value }: ErrorNumberCellRendererProps) => {
   const props = value.length > 0 ? { color: "error" } : {};
-  const errorCount = value.length > 0 ? value.reduce((totalJobRunErrors, error) => totalJobRunErrors + Number(error.count), 0) : "-";
+
+  const getErrorCount = () => {
+    if (value.length === 0) {
+      return "-";
+    } else {
+      return value.reduce((totalJobRunErrors, error) => totalJobRunErrors + Number(error.count), 0);
+    }
+  };
+  const errorCount = useMemo(() => getErrorCount(), [value]);
 
   return <Span {...props}>{errorCount}</Span>;
 };
