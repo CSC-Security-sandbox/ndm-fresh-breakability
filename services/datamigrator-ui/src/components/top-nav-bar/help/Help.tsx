fca20df69e1ce2072cf8099f3e6f3350a4598096
gm67249 <gm67249@netapp.com>
2025-06-04 18:43:38 +0530

    NDM-1489-About NetApp Data Migrator

--- services/datamigrator-ui/src/components/top-nav-bar/help/Help.tsx
+++ services/datamigrator-ui/src/components/top-nav-bar/help/Help.tsx
@@ -6,41 +6,39 @@ import { useState } from "react";
 import AboutNDM from "@components/top-nav-bar/help/about_ndm.tsx";
 import { Collapse } from "@mui/material";
 
-
 const Help = () => {
-    console.log("HelpItems ", HELP_ITEMS);
-  const [isHelpListVisible, setIsHelpListVisible] =
-    useState<boolean>(true);
+  const [isHelpListVisible, setIsHelpListVisible] = useState<boolean>(true);
   const handleClose = () => {
     setIsHelpListVisible(true);
   };
+
   return (
     <>
-    <Collapse in={isHelpListVisible} mountOnEnter unmountOnExit>
-    <Card className="h-full w-[40rem]">
-      <Layout.Page>
-        <WizardHeader Icon={HelpIcon} label="Help" />
-        <Layout.Content
-          style={{ padding: 20, backgroundColor: "var(--light-bg)" }}
-        >
+      <Collapse in={isHelpListVisible} mountOnEnter unmountOnExit>
+        <Card className="h-full w-[40rem]">
+          <Layout.Page>
+            <WizardHeader Icon={HelpIcon} label="Help" />
+            <Layout.Content
+              style={{ padding: 20, backgroundColor: "var(--light-bg)" }}
+            >
               {HELP_ITEMS.map((item) => (
-              <Box
-              className="flex flex-row justify-between items-center p-3 border-b cursor-pointer hover:bg-slate-100 hover:text-text-title transition-all duration-100"
-              key={item.id}
-              onClick={() => setIsHelpListVisible(item.id !== 1)}
-              >
-               {item.name}
-               </Box>
-                ))}
-        </Layout.Content>
-      </Layout.Page>
-    </Card>
-    </Collapse>
-  <Collapse in={!isHelpListVisible} mountOnEnter unmountOnExit>
-    <Box className="flex justify-around">
-      <AboutNDM closeAction={handleClose}></AboutNDM>
-    </Box>
-  </Collapse>
+                <Box
+                  className="flex flex-row justify-between items-center p-3 border-b cursor-pointer hover:bg-slate-100 hover:text-text-title transition-all duration-100"
+                  key={item.id}
+                  onClick={() => setIsHelpListVisible(item.id !== 1)}
+                >
+                  {item.name}
+                </Box>
+              ))}
+            </Layout.Content>
+          </Layout.Page>
+        </Card>
+      </Collapse>
+      <Collapse in={!isHelpListVisible} mountOnEnter unmountOnExit>
+        <Box className="flex justify-around">
+          <AboutNDM closeAction={handleClose}></AboutNDM>
+        </Box>
+      </Collapse>
     </>
   );
 };
