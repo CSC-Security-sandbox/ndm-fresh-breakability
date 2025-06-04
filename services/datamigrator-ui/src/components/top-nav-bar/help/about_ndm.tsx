d5d8e40debdb925c94551b5dcb9f7fd7fba85298
gm67249 <gm67249@netapp.com>
2025-06-04 19:16:54 +0530

    NDM-1489-About NetApp Data Migrator

--- services/datamigrator-ui/src/components/top-nav-bar/help/about_ndm.tsx
+++ services/datamigrator-ui/src/components/top-nav-bar/help/about_ndm.tsx
@@ -1,14 +1,16 @@
 import Box from "@/components/container/Box";
-import { InfoIcon } from "@netapp/bxp-style/react-icons/Notification"
+import { InfoIcon } from "@netapp/bxp-style/react-icons/Notification";
 import {
   Button,
   Card,
-  Layout, Text, WizardFooter,
-  WizardHeader
+  Layout,
+  Text,
+  WizardFooter,
+  WizardHeader,
 } from "@netapp/bxp-design-system-react";
 import React from "react";
 
-const AboutNDM = ({ closeAction}) => {
+const AboutNDM = ({ closeAction }) => {
   const handleClose = () => {
     closeAction();
   };
@@ -20,14 +22,19 @@ const AboutNDM = ({ closeAction}) => {
           <Card className="p-10 flex flex-col gap-6">
             <Box className="p-10 flex flex-col gap-2">
               <Box className="flex gap-4">
-                <Text><b>Builder Version:</b> 2025.06.03-alpha</Text>
+                <Text>
+                  <b>Builder Version:</b> 2025.06.03-alpha
+                </Text>
               </Box>
               <Box className="flex gap-4">
-                <Text><b>Contact Us:</b> <u className="text-blue-500">niharika@netapp.com</u></Text>
+                <Text>
+                  <b>Contact Us:</b>{" "}
+                  <u className="text-blue-500">niharika@netapp.com</u>
+                </Text>
               </Box>
             </Box>
           </Card>
-          <WizardFooter className="" style={{}}>
+          <WizardFooter>
             <Box className="flex w-full justify-end gap-4 mr-4">
               <Button
                 style={{ width: 150 }}
