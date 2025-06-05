0f75b1d511d1d36bf4112ec4e02542b906c0563c
abhishek buragadda <aburagad@netapp.com>
2025-06-05 13:57:33 +0530

    add workflow tests

--- services/worker/src/workflows/discovery/core/discovery-job-workflow.ts
+++ services/worker/src/workflows/discovery/core/discovery-job-workflow.ts
@@ -1,9 +1,9 @@
 import { continueAsNew, ContinueAsNew, proxyActivities } from '@temporalio/workflow';
-import { CommonActivityService } from 'src/activities/common/common.service';
-import { DiscoveryActivity } from 'src/activities/discovery/discovery.activities';
-import { DiscoveryScanActivity } from 'src/activities/discovery/discovery.core.activity';
-import { DiscoverPathOutput } from 'src/activities/discovery/discovery.type';
-import { JobRunStatus } from 'src/activities/discovery/enums';
+import { CommonActivityService } from '../../../activities/common/common.service';
+import { DiscoveryActivity } from '../../../activities/discovery/discovery.activities';
+import { DiscoveryScanActivity } from '../../../activities/discovery/discovery.core.activity';
+import { DiscoverPathOutput } from '../../../activities/discovery/discovery.type';
+import { JobRunStatus } from '../../../activities/discovery/enums';
 import * as wf from '@temporalio/workflow';
 import { ActivityFailure } from '@temporalio/workflow';
 
