6dda8ce00c31afdaa9dffeca576f8681a0c1bacb
Rutuja Deshmukh <rutuja.deshmukh@calfus.com>
2025-06-06 12:32:57 +0530

    GCNV Flex Feature

---
+++ services/datamigrator-ui/src/modules/storage-servers/file-server/components/cellRenderer/ExportPathSourceCellRenderer.tsx
@@ -0,0 +1,13 @@
+import { BlueXpTableRowType, ConfigListTypeApiType } from "@/types/app.type";
+import { FILE_SERVER_LIST_ENUM } from "@modules/storage-servers/file-server/file-server.constant";
+import { memo } from "react";
+
+const ExportPathSourceCellRenderer = (
+  props: BlueXpTableRowType<ConfigListTypeApiType, ConfigListTypeApiType>
+) => {
+  return (
+    <> {FILE_SERVER_LIST_ENUM[props.row.fileServers[0].exportPathSource]}</>
+  );
+};
+
+export default memo(ExportPathSourceCellRenderer);
