a00d893b81226c17c1ed77ecf48fa54c18eb2de6
Rutuja Deshmukh <rutuja.deshmukh@calfus.com>
2025-06-06 12:32:57 +0530

    GCNV Flex Feature

---
+++ services/datamigrator-ui/src/modules/storage-servers/file-server/file-server-overview/bulk-manual-upload/components/BulkManualUploadModalFooter.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@netapp/bxp-design-system-react";
+import { BlueXpFormType } from "@/types/app.type";
+import { BulkManualUploadModalContentPropsType } from "@modules/storage-servers/file-server/file-server-overview/bulk-manual-upload/bulk-manual-upload-file.types";
+
+export const BulkManualUploadModalFooter = (
+  form: BlueXpFormType<BulkManualUploadModalContentPropsType>,
+  onSubmit: () => Promise<void>,
+  handleResetAndClose: () => void
+) => {
+  return (
+    <>
+      <Button color="secondary" onClick={handleResetAndClose}>
+        Cancel
+      </Button>
+      <Button
+        disabled={!form?.formState?.exportPathSource || !form?.isValid}
+        onClick={onSubmit}
+      >
+        Submit
+      </Button>
+    </>
+  );
+};
