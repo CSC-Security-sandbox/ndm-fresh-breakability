253e23663dc322f414ae76e02808e3acca6babd2
Rutuja Deshmukh <rutuja.deshmukh@calfus.com>
2025-06-09 15:10:21 +0530

    Path table validations

--- services/datamigrator-ui/src/modules/storage-servers/file-server/file-server-overview/bulk-manual-upload/components/BulkManualUploadFile.tsx
+++ services/datamigrator-ui/src/modules/storage-servers/file-server/file-server-overview/bulk-manual-upload/components/BulkManualUploadFile.tsx
@@ -1,21 +1,26 @@
 import { Button } from "@netapp/bxp-design-system-react";
 import { Box } from "@components/container";
 import { handleDownloadTemplate } from "@modules/storage-servers/file-server/file-server-overview/bulk-migrate/bulk-migrate.utils";
-import useBulkManualUpload from "@modules/storage-servers/file-server/file-server-overview/bulk-manual-upload/useBulkManualUpload";
 import { BulkManualUploadPropsType } from "@modules/storage-servers/file-server/file-server-overview/bulk-manual-upload/bulk-manual-upload-file.types";
+import { BulkManualUpload } from "@modules/storage-servers/file-server/file-server-overview/bulk-manual-upload/BulkManualUpload";
 
 const BulkManualUploadFile = ({
   fileServerDetails,
   allExportPaths,
 }: BulkManualUploadPropsType) => {
-  const { openUploadModal, downloadTemplate, buttonName } = useBulkManualUpload(
+  const { openUploadModal, downloadTemplate, buttonName } = BulkManualUpload(
     fileServerDetails,
     allExportPaths
   );
 
   return (
     <Box className="flex gap-2 justify-end">
-      <Button onClick={openUploadModal}>{buttonName}</Button>
+      <Button
+        disabled={!fileServerDetails?.isRefreshAvailable}
+        onClick={openUploadModal}
+      >
+        {buttonName}
+      </Button>
       <Button
         color="secondary"
         onClick={() =>
