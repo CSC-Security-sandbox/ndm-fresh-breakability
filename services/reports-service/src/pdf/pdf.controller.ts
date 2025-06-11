7bfb07f69907f8c4a4acc43d1a2d306fe2065839
GRamyaCal <ramyag1@netapp.com>
2025-06-11 17:18:41 +0530

    code format

--- services/reports-service/src/pdf/pdf.controller.ts
+++ services/reports-service/src/pdf/pdf.controller.ts
@@ -45,6 +45,7 @@ export class PdfController {
           message: error.message || 'Failed to generate the PDF report. Please try again later.',
           name: error.name || 'Error',
         };
+        
         res.status(sanitizedError.status).send(sanitizedError);
       } 
     } 
