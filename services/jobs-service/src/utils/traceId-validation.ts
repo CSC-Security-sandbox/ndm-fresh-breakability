1e8f8728916a01753b3d07bf924ae53db006ca55
GRamyaCal <ramyag1@netapp.com>
2025-06-12 12:39:44 +0530

    removed unwanted imports

--- services/jobs-service/src/utils/traceId-validation.ts
+++ services/jobs-service/src/utils/traceId-validation.ts
@@ -1,6 +1,3 @@
-import * as parser from 'cron-parser';
-import { JobType } from "src/constants/enums";
-
 export const traceIdValidation = (traceId: string) => {
   // Validating traceId: It must be alphanumeric and 36 characters as it is a UUID
   const traceIdRegex = /^[a-zA-Z0-9_-]{36}$/;
