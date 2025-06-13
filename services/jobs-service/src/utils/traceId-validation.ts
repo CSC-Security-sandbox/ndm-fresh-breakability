a82dd5e684834daaaab48d8ff697e27075ba4338
GRamyaCal <ramyag1@netapp.com>
2025-06-13 20:53:26 +0530

    sanitising and validating traceId

--- services/jobs-service/src/utils/traceId-validation.ts
+++ services/jobs-service/src/utils/traceId-validation.ts
@@ -1,4 +1,5 @@
 export const traceIdValidation = (traceId: string) => {
   const traceIdRegex = /^[a-zA-Z0-9_-]{36}$/;
-  return traceIdRegex.test(traceId);
+  const sanitizedTraceId = traceId.replace(/[^a-zA-Z0-9-]/g, '');
+  return traceIdRegex.test(sanitizedTraceId) && sanitizedTraceId.length === 36;
 }
