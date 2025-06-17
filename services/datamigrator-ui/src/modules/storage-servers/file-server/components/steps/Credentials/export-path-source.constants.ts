ad01bbdea8ee5507f4a022f49ccd05368c9940c4
Rutuja Deshmukh <rutuja.deshmukh@calfus.com>
2025-06-17 11:32:56 +0530

    optimised code

--- services/datamigrator-ui/src/modules/storage-servers/file-server/components/steps/Credentials/export-path-source.constants.ts
+++ services/datamigrator-ui/src/modules/storage-servers/file-server/components/steps/Credentials/export-path-source.constants.ts
@@ -7,3 +7,6 @@ export const RADIO_OPTIONS = [
 
 export const MANUAL_UPLOAD_INFO =
   "To manually add export paths, complete the file server setup first. Then, go to the File Server Overview page to upload the file containing the required export paths.";
+
+export const EXPORT_PATH_SOURCE_NOTE =
+  "Use manual upload if showmount is not supported, such as with GCNV Flex service, or if you prefer to upload the export paths manually.";
