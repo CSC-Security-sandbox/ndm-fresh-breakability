66514f9ee922f39d3d11d010f264aec9cc131eec
GRamyaCal <ramyag1@netapp.com>
2025-06-17 16:07:37 +0530

    removed the changes added in logger.go as with new code changes from main the codeql is not exisit

--- ndm-api-tests/utils/logger.go
+++ ndm-api-tests/utils/logger.go
@@ -1,37 +1,19 @@
 package utils
 
-import (
-    "log"
-    "regexp"
-)
+import "log"
 
-var compiledPatterns = []*regexp.Regexp{
-   regexp.MustCompile(`(?i)(password\s*[:=]?\s*)\S+`),
-   regexp.MustCompile(`(?i)(token\s*[:=]?\s*)\S+`),
-   regexp.MustCompile(`(?i)(email\s*[:=]?\s*)\S+`),
-}
-
-func sanitize(msg string) string {
-    for _, re := range compiledPatterns {
-        msg = re.ReplaceAllString(msg, "$1[REDACTED]")
-    }
- 
-    return msg
-}
-    
 func LogDebug(msg string) {
-    // sanitisedMessage := sanitize(msg)
     log.Print("[DEBUG] " + msg)
 }
 
 func LogError(msg string, err ...error) {
-	if len(err) > 0 && err[0] != nil {
-		log.Print("[ERROR] " + msg + " | Error: " + err[0].Error())
-	} else {
-		log.Print("[ERROR] " + msg)
-	}
+    if len(err) > 0 && err[0] != nil {
+        log.Print("[ERROR] " + msg + " | Error: " + err[0].Error())
+    } else {
+        log.Print("[ERROR] " + msg)
+    }
 }
 
 func LogFatalf(format string, args ...interface{}) {
-	log.Fatalf("[FATAL] "+format, args...)
+    log.Fatalf("[FATAL] "+format, args...)
 }
