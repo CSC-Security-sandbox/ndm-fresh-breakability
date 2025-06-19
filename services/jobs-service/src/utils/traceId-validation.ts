91de4061efa34ca959b2e7e29f4652918dcc56fc
Ramya G <ramya.g@IBM-K2W7L9N43R.local>
2025-06-19 16:04:00 +0530

    updated the traceID validation with proper UUID valdiation

--- services/jobs-service/src/utils/traceId-validation.ts
+++ services/jobs-service/src/utils/traceId-validation.ts
@@ -1,5 +1,4 @@
-export const traceIdValidation = (traceId: string) => {
-  const traceIdRegex = /^[a-zA-Z0-9_-]{36}$/;
-  const sanitizedTraceId = traceId.replace(/[^a-zA-Z0-9-]/g, '');
-  return traceIdRegex.test(sanitizedTraceId) && sanitizedTraceId.length === 36;
-}
+export const traceIdValidation = (traceId: string): boolean => {
+  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return uuidRegex.test(traceId);
+};
