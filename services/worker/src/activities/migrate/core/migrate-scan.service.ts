7b6fa5aa4cdcb1777572cde6081e01b78e260508
abhishek buragadda <aburagad@netapp.com>
2025-06-24 17:58:03 +0530

    renaming and identifying places for error handling

--- services/worker/src/activities/migrate/core/migrate-scan.service.ts
+++ services/worker/src/activities/migrate/core/migrate-scan.service.ts
@@ -33,29 +33,29 @@ export class MigrateScanService {
     
     }
     async publishCommands({ jobContext, commands}: PublishCommandInput)  {
+        //TODO: make bulk publish to command stream. 
         for(const command of commands)
             await jobContext.publishToCommandStream(command);
-
     }
 
+    async getDirContents(path: string): Promise<Set<string>>{
+        let content = new Set<string>();
+        try{
+            content = new Set<string>( await this.getDirectoryContents(path)); 
+        }catch(error){
+                this.logger.error(`Error reading directory ${path}: ${error}`);
+                throw error;
+        }
+        return content;
+    }
 
     async scanDirectory({excludePatterns = [], jobContext, sourcePath, sourcePrefix, targetPath, jobRunId, skipFile, }: ScanDirectoryInput): Promise<ScanDirectoryOutput> { 
         const output: ScanDirectoryOutput = {jobRunId, fileCount: 0, dirCount: 0, subDirs: []};
         let sourceContent: Set<string> =  new Set(), targetContent: Set<string> = new Set();
         let commands: Command[] = [];
-        try {
-            sourceContent = new Set<string>(await this.getDirectoryContents(sourcePath));
-        }catch(error) {
-            this.logger.error(`Error reading source directory ${sourcePath}: ${error}`);
-        }
 
-        try {
-            targetContent = new Set<string>(await this.getDirectoryContents(targetPath));           
-        }
-        catch(error) {
-           this.logger.error(`Error reading target directory ${targetPath}: ${error}`);
-
-        }
+        sourceContent = await this.getDirContents(sourcePath);
+        targetContent = await this.getDirContents(targetPath);
         for (const item of sourceContent) {
             try {
                 const sourceContentPath = path.join(sourcePath, item);
@@ -102,6 +102,7 @@ export class MigrateScanService {
                 
             }catch(error) {
                 this.logger.error(`Error processing item ${item} in directory ${sourcePath}: ${error}`);
+                //TODO: if fatal error , raise non-retryable error else just throw error back. 
             }
         }
         if (commands.length > 0) {
@@ -122,37 +123,43 @@ export class MigrateScanService {
             subDirs: [],
             jobRunId: jobRunId
         }
-        const jobContext: JobManagerContext = await this.redisService.getJobManagerContext(jobRunId);
-        const jobConfig = jobContext.getJobConfig()
-        this.logger.debug(`Job Config is : ${jobConfig}`)
+        try{                           
+            const jobContext: JobManagerContext = await this.redisService.getJobManagerContext(jobRunId);
+            const jobConfig = jobContext.getJobConfig()
+            this.logger.debug(`Job Config is : ${jobConfig}`)
 
-        const baseSourcePrefixPath = basePrefix(jobRunId, jobConfig.sourceFileServer.pathId);
-        const baseTargetPrefixPath = basePrefix(jobRunId, jobConfig.destinationFileServer.pathId);
-        const excludePatterns = jobContext.jobConfig.options?.excludeFilePattern ? jobContext.jobConfig.options.excludeFilePattern.split(",") : [];
-        const skipFile = jobContext.jobConfig.options?.skipsFilesModifiedInLast ?? '';
+            const baseSourcePrefixPath = basePrefix(jobRunId, jobConfig.sourceFileServer.pathId);
+            const baseTargetPrefixPath = basePrefix(jobRunId, jobConfig.destinationFileServer.pathId);
+            const excludePatterns = jobContext.jobConfig.options?.excludeFilePattern ? jobContext.jobConfig.options.excludeFilePattern.split(",") : [];
+            const skipFile = jobContext.jobConfig.options?.skipsFilesModifiedInLast ?? '';
+            
+            for (let i = 0; i < dirsToScan.length; i += this.maxConcurrency) {
+                const batch = dirsToScan.slice(i, i + this.maxConcurrency);
         
-        for (let i = 0; i < dirsToScan.length; i += this.maxConcurrency) {
-            const batch = dirsToScan.slice(i, i + this.maxConcurrency);
-    
-            await Promise.allSettled(
-                batch.map(async (dirPath) => {
-                    const scanDirectoryInput : ScanDirectoryInput = {
-                        excludePatterns,
-                        sourcePath: `${baseSourcePrefixPath}${dirPath}`,
-                        sourcePrefix: baseSourcePrefixPath,
-                        targetPath: `${baseTargetPrefixPath}${dirPath}`,
-                        jobRunId,
-                        jobContext,
-                        skipFile,
-                    }
-                    const result = await this.scanDirectory(scanDirectoryInput);
-                    output.fileCount += result.fileCount;
-                    output.dirCount += result.dirCount;
-                    output.subDirs.push(...result.subDirs);
-                })
-            )
-        }
-        clearInterval(heartbeatInterval);
+                await Promise.allSettled(
+                    batch.map(async (dirPath) => {
+                        const scanDirectoryInput : ScanDirectoryInput = {
+                            excludePatterns,
+                            sourcePath: `${baseSourcePrefixPath}${dirPath}`,
+                            sourcePrefix: baseSourcePrefixPath,
+                            targetPath: `${baseTargetPrefixPath}${dirPath}`,
+                            jobRunId,
+                            jobContext,
+                            skipFile,
+                        }
+                        const result = await this.scanDirectory(scanDirectoryInput);
+                        output.fileCount += result.fileCount;
+                        output.dirCount += result.dirCount;
+                        output.subDirs.push(...result.subDirs);
+                    })
+                )
+            }
+        }catch(error){
+            //TODO: if fatal error , raise a non retryable error. else just throw the error back . 
+        }        
+        finally{
+            clearInterval(heartbeatInterval);
+        }        
         return output
     }
 
