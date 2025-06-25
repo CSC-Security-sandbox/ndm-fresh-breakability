e8a0e4b0a36da1cb777bcacd6e25ec00c98a1910
gm67249 <gm67249@netapp.com>
2025-06-25 09:43:20 +0530

    NDM-1389- Report

--- services/reports-service/src/config/app.config.ts
+++ services/reports-service/src/config/app.config.ts
@@ -1,16 +1,16 @@
-import { registerAs } from '@nestjs/config';
+import { registerAs } from "@nestjs/config";
 
 export default registerAs(
-  'app',
+  "app",
   (): Record<string, any> => ({
     http: {
-      host: process.env.APP_HOST || '0.0.0.0',
-      port: parseInt(process.env.APP_PORT) || 3000,
+      host: process.env.APP_HOST || "0.0.0.0",
+      port: parseInt(process.env.APP_PORT) || 3003,
     },
     rabbitmq: {
-        urls: process.env.RABBITMQ_URLS?.split(',') || [],
-        durable: process.env.RABBITMQ_QUEUE_IS_DURABLE || true,
-        reportsQueue: process.env.REPORTS_QUEUE || '',
-    }
+      urls: process.env.RABBITMQ_URLS?.split(",") || [],
+      durable: process.env.RABBITMQ_QUEUE_IS_DURABLE || true,
+      reportsQueue: process.env.REPORTS_QUEUE || "",
+    },
   }),
 );
