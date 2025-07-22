5957a374327e26d331a9feb44f039f9f3e1a8003
Ashish Sinha <si82974@netapp.com>
2025-07-22 17:00:36 +0530

    feat(redis) use connection pool ion redis (#282)

--- services/worker/src/redis/redis.service.ts
+++ services/worker/src/redis/redis.service.ts
@@ -1,93 +1,44 @@
-import { JobContext, JobContextFactory } from '@netapp-cloud-datamigrate/jobs-lib';
+import { JobContextFactory, RedisUtils } from '@netapp-cloud-datamigrate/jobs-lib';
 import { JobState } from '@netapp-cloud-datamigrate/jobs-lib/dist/types/job-state';
-import { Inject, Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { createClient, RedisClientType } from 'redis';
-import { LoggerService, LoggerFactory } from '@netapp-cloud-datamigrate/logger-lib';
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+
+import { RedisClientType } from 'redis';
 
 @Injectable()
-export class RedisService implements OnModuleInit, OnModuleDestroy {
-  private client: RedisClientType;
-  private readonly logger : LoggerService;
+export class RedisService  implements OnModuleInit, OnModuleDestroy {
 
-  constructor (
-    @Inject(LoggerFactory) loggerFactory: LoggerFactory,
-  ) {
-    this.logger = loggerFactory.create(RedisService.name);
+  private client: RedisClientType;
+  private redisUtils: RedisUtils;
+  private readonly logger = new Logger(RedisService.name);
+
+  constructor() {
+    this.redisUtils = new RedisUtils();
   }
 
   async onModuleInit(): Promise<void> {
-    await this.createClient();
+    await this.redisUtils.getClient();
   }
 
   async onModuleDestroy(): Promise<void> {
-    if (this.client && this.client.isOpen) {
-      await this.client.quit();
-      this.logger.log('Redis client disconnected');
-    }
-  }
-
-  async createClient(): Promise<void> {
-    if (this.client && this.client.isOpen) {
-      return;
-    }
-    
-    const redisClientOptions: any = {
-      url: `redis://${process.env.REDIS_HOST || '127.0.0.1'}:${process.env.REDIS_PORT || 6379}`,
-    };
-
-    if (process.env.REDIS_USERNAME && process.env.REDIS_PASSWORD) {
-      redisClientOptions.username = process.env.REDIS_USERNAME;
-      redisClientOptions.password = process.env.REDIS_PASSWORD;
-    }
-
-    this.logger.log(`Connecting to Redis at ${redisClientOptions.url}`);
-    this.client = createClient(redisClientOptions);
-
-    this.client.on('error', (error) => {
-      this.logger.error(`Redis connection error: ${error}`);
-    });
-
-    this.client.on('connect', () => {
-      this.logger.log('Connected to Redis');
-    });
-
-    await this.client.connect();
-  }
-
-  private async ensureClient(): Promise<void> {
-    if (!this.client || !this.client.isOpen) {
-      this.logger.warn('Redis client not initialized. Attempting to reconnect...');
-      await this.createClient();
-    }
-  }
-
-  getClient(): RedisClientType {
-    if (!this.client || !this.client.isOpen) {
-      throw new Error('Redis client is not initialized yet.');
-    }
-    return this.client;
+    await this.redisUtils.closePool();
   }
 
   async getJobContext(traceId: string) {
-    await this.ensureClient();
     const contextProvider = JobContextFactory.getProvider('redis', this.client);
     return await contextProvider.getJobContext(traceId);
   }
 
   async getJobManagerContext(traceId: string) {
-    await this.ensureClient();
     const contextProvider = JobContextFactory.getJobManagerProvider('redis', this.client);
     return await contextProvider.getContext(traceId);
   }
 
   async getSpeedTestJobContext(traceId: string) {
-    await this.ensureClient();
     const contextProvider = JobContextFactory.getSpeedTestProvider('redis', this.client);
     return await contextProvider.getJobContext(traceId);
   }
 
   async setJobContext(traceId: string, jobContext: any) {
-    await this.ensureClient();
     const serializedContext = jobContext.serialize();
     await this.client.set(traceId, serializedContext);
   }
@@ -117,7 +68,7 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
   }
 
   async getMemoryInfo(): Promise<{ used_memory: number; total_system_memory: number }> {
-    await this.ensureClient();
+    this.client = await this.redisUtils.getClient();
     const memoryInfo = await this.client.info('memory');
     const parsedInfo = this.parseMemoryStats(memoryInfo);
     return parsedInfo;
