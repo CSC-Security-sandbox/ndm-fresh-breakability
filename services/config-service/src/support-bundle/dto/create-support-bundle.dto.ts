dfd7a3d22a112d9718bcb7abfabc27c615edd8e5
AniketCalfus <aniket.darekar@calfus.com>
2025-07-26 20:03:50 +0530

    updated the zip creating logic now it contains both cp and worker logs

--- services/config-service/src/support-bundle/dto/create-support-bundle.dto.ts
+++ services/config-service/src/support-bundle/dto/create-support-bundle.dto.ts
@@ -1,14 +1,65 @@
-import { IsOptional, IsArray, IsUUID, IsDateString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsOptional,
+  IsArray,
+  IsUUID,
+  IsDateString,
+  ValidateNested,
+  IsNotEmpty,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class CreateSupportBundleDTO {
+export class ProjectWorkerMapDto {
+  @ApiPropertyOptional({
+    description: 'Project ID',
+    type: String,
+    format: 'uuid',
+  })
+  @IsOptional()
+  @IsUUID()
+  projectId?: string;
+
+  @ApiPropertyOptional({
+    description: 'List of Worker IDs',
+    type: [String],
+    format: 'uuid',
+  })
   @IsOptional()
   @IsArray()
   @IsUUID('all', { each: true })
-  projectIds?: string[];
+  workerIds?: string[];
+}
 
+export class CreateSupportBundleDTO {
+  @ApiProperty({ description: 'User ID', type: String, format: 'uuid' })
+  @IsUUID()
+  @IsNotEmpty()
+  userId: string;
+
+  @ApiProperty({ description: 'Start date', type: String, format: 'date-time' })
   @IsDateString()
   startDate: string;
 
+  @ApiProperty({ description: 'End date', type: String, format: 'date-time' })
   @IsDateString()
   endDate: string;
+
+  @ApiPropertyOptional({
+    description: 'Mapping of projects to their worker IDs',
+    type: [ProjectWorkerMapDto],
+  })
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ProjectWorkerMapDto)
+  projectWorkerMap?: ProjectWorkerMapDto[];
+
+  @ApiPropertyOptional({
+    description: 'List of other metrics to include',
+    type: [String],
+    example: ['state data', 'inventory data'],
+  })
+  @IsOptional()
+  @IsArray()
+  otherMetrics?: string[];
 }
