31f3eea80fe0640bb23a2e688b4a50e6ad63a69e
AniketCalfus <aniket.darekar@calfus.com>
2025-07-30 00:14:24 +0530

    unit test cases added

--- services/support-service/src/temporal/workflows.ts
+++ services/support-service/src/temporal/workflows.ts
@@ -1,2 +1,3 @@
 export { SupportBundleWorkflow } from './parent-workflow';
 export { LogGeneratorWorkflow } from './child-workflows/log-generator-workflow';
+export { ErrorCsvGeneratorWorkflow } from './child-workflows/error-csv-generator-workflow';
