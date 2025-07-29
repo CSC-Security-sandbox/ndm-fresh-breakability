1237a3201767dc7f4000cf07283bb0f953e3ce4d
AniketCalfus <aniket.darekar@calfus.com>
2025-07-29 18:29:42 +0530

    made database connection and added entities

--- services/support-service/src/app.module.ts
+++ services/support-service/src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { WorkerModule } from './worker/worker.module';
 import { ActivitiesModule } from './activities/activities.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import temporalConfig from './config/temporal.config';
 import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ load: [appConfig, temporalConfig] }),
+    ConfigModule.forRoot({ isGlobal: true, load: [appConfig, databaseConfig, temporalConfig] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+       configService.getOrThrow<TypeOrmModuleOptions>('typeorm'),
+    }),
     WorkerModule,
     ActivitiesModule,
   ],
@@ -15,3 +23,4 @@ import appConfig from './config/app.config';
   providers: [],
 })
 export class AppModule {}
+
