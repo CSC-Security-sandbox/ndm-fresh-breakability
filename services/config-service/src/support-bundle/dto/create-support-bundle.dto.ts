7b1a6246d195b59bca97ac2293c0c27298a1ab24
AniketCalfus <aniket.darekar@calfus.com>
2025-07-30 22:56:17 +0530

    added apis to interact with ui

--- services/config-service/src/support-bundle/dto/create-support-bundle.dto.ts
+++ services/config-service/src/support-bundle/dto/create-support-bundle.dto.ts
@@ -2,32 +2,8 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsOptional,
   IsArray,
-  IsUUID,
   IsDateString,
-  ValidateNested,
 } from 'class-validator';
-import { Type } from 'class-transformer';
-
-export class ProjectWorkerMapDto {
-  @ApiPropertyOptional({
-    description: 'Project ID',
-    type: String,
-    format: 'uuid',
-  })
-  @IsOptional()
-  @IsUUID()
-  projectId?: string;
-
-  @ApiPropertyOptional({
-    description: 'List of Worker IDs',
-    type: [String],
-    format: 'uuid',
-  })
-  @IsOptional()
-  @IsArray()
-  @IsUUID('all', { each: true })
-  workerIds?: string[];
-}
 
 export class CreateSupportBundleDTO {
   @ApiProperty({ description: 'Start date', type: String, format: 'date-time' })
@@ -37,15 +13,6 @@ export class CreateSupportBundleDTO {
   @ApiProperty({ description: 'End date', type: String, format: 'date-time' })
   @IsDateString()
   endDate: string;
-
-  @ApiProperty({
-    description: 'Mapping of projects to their worker IDs',
-    type: [ProjectWorkerMapDto],
-  })
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => ProjectWorkerMapDto)
-  projectWorkerMap: ProjectWorkerMapDto[];
 
   @ApiPropertyOptional({
     description: 'List of other metrics to include',
