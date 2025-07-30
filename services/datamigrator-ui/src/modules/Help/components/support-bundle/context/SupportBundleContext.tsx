d13d0634cc5fc13825463a8ea92c27820bff1040
Rutuja Deshmukh <rutuja.deshmukh@calfus.com>
2025-07-31 00:47:27 +0530

    changes without project id

--- services/datamigrator-ui/src/modules/Help/components/support-bundle/context/SupportBundleContext.tsx
+++ services/datamigrator-ui/src/modules/Help/components/support-bundle/context/SupportBundleContext.tsx
@@ -3,39 +3,28 @@ import { useSelector, useDispatch } from "react-redux";
 import { useForm } from "@netapp/bxp-design-system-react";
 import {
   useLazyDownloadSupportBundleQuery,
-  useFetchProjectWithWorkerQuery,
   useGenerateSupportBundleMutation,
   useCheckBundleReadyStatusQuery,
 } from "@api/configApi";
 import { formatDateToYMD } from "@/utils/dateFormatter";
-import { createAndDownloadBlob, getMimeType } from "@modules/jobs/jobs.utils";
 import { notify } from "@components/notification/NotificationWrapper";
 import { RootStateType } from "@store/store";
 import { setLastGeneratedBundlePayload } from "@store/reducer/appSlice";
-import { buildProjectWorkerMap } from "@modules/Help/components/support-bundle/utils/support-bundle.utils";
 import {
   SupportBundleContextType,
   SupportBundlePayloadType,
 } from "@modules/Help/components/support-bundle/types/support-bundle.types";
 import { SupportBundleContext } from "@modules/Help/components/support-bundle/context/context";
-import { useTreeSelect } from "@modules/Help/components/support-bundle/hooks/useTreeSelect";
 import { INITIAL_FORM_STATE } from "@modules/Help/components/support-bundle/constants/support-bundle.constant";
+import { createAndDownloadBlob, getMimeType } from "@modules/jobs/jobs.utils";
 
 export const SupportBundleProvider = ({
   children,
 }: React.PropsWithChildren) => {
-  const dispatch = useDispatch();
   const form = useForm(INITIAL_FORM_STATE);
-  const {
-    selectedItems,
-    treeSelectStyles,
-    handleSelectionChange,
-    wrapperClass,
-  } = useTreeSelect();
 
   const [generateBundle] = useGenerateSupportBundleMutation();
   const [downloadBundle] = useLazyDownloadSupportBundleQuery();
-  const { data: projectWorkerData } = useFetchProjectWithWorkerQuery();
   const { data: bundleReadyStatus } = useCheckBundleReadyStatusQuery(null, {
     pollingInterval: Number(
       window?.env?.VITE_TIME_INTERVAL || import.meta.env.VITE_TIME_INTERVAL
@@ -43,6 +32,7 @@ export const SupportBundleProvider = ({
     skipPollingIfUnfocused: true,
   });
 
+  const dispatch = useDispatch();
   const permissionData = useSelector(
     (state: RootStateType) => state?.permissionSlice?.userPermissions
   );
@@ -55,23 +45,14 @@ export const SupportBundleProvider = ({
   };
 
   const isFormDataDifferentFromLastBundle = () => {
-    if (!lastGeneratedBundlePayload || !form?.formState || !projectWorkerData)
-      return false;
+    if (!lastGeneratedBundlePayload || !form?.formState) return false;
 
     try {
-      const projectWorker = Array.isArray(form?.formState?.project_worker)
-        ? form?.formState?.project_worker
-        : [];
-
       const otherMetrics = form?.formState?.other_metrics?.label
         ? [form?.formState?.other_metrics?.label]
         : [];
 
       const currentPayload = {
-        projectWorkerMap: buildProjectWorkerMap(
-          { ...form?.formState, project_worker: projectWorker },
-          projectWorkerData
-        ),
         startDate: formatDateToYMD(form?.formState?.start_date),
         endDate: formatDateToYMD(form?.formState?.end_date),
         otherMetrics,
@@ -88,24 +69,16 @@ export const SupportBundleProvider = ({
   };
 
   const validateForm = () => {
-    const { start_date, end_date, project_worker } = form?.formState || {};
-    const isFormValid =
-      start_date &&
-      end_date &&
-      Array.isArray(project_worker) &&
-      project_worker.length > 0;
+    const { start_date, end_date } = form?.formState || {};
+    const isFormValid = start_date && end_date;
+
     updateFormField("isValid", isFormValid);
   };
 
   const formState = form?.formState || {};
-  const { isValid, isProcessing, start_date, end_date, project_worker } =
-    formState;
+  const { isValid, isProcessing, start_date, end_date } = formState;
 
-  const hasFormData = Boolean(
-    start_date ||
-      end_date ||
-      (Array.isArray(project_worker) && project_worker.length)
-  );
+  const hasFormData = Boolean(start_date || end_date);
   const isBundleReady = bundleReadyStatus?.isBundleReady;
   const isFormDataChanged = isFormDataDifferentFromLastBundle();
 
@@ -148,10 +121,6 @@ export const SupportBundleProvider = ({
       : [];
 
     const payload: SupportBundlePayloadType = {
-      projectWorkerMap: buildProjectWorkerMap(
-        form?.formState,
-        projectWorkerData
-      ),
       startDate: formatDateToYMD(form?.formState?.start_date),
       endDate: formatDateToYMD(form?.formState?.end_date),
       otherMetrics,
@@ -169,15 +138,10 @@ export const SupportBundleProvider = ({
   };
 
   useEffect(() => {
-    if (selectedItems) updateFormField("project_worker", selectedItems);
-  }, [selectedItems]);
-
-  useEffect(() => {
     validateForm();
   }, [
     form?.formState?.start_date,
     form?.formState?.end_date,
-    form?.formState?.project_worker,
     form?.formState?.other_metrics,
   ]);
 
@@ -192,11 +156,7 @@ export const SupportBundleProvider = ({
 
   const contextValue: SupportBundleContextType = {
     form,
-    projectWorkerData,
     bundleReadyStatus,
-    treeSelectStyles,
-    handleSelectionChange,
-    wrapperClass,
     handleDateChange,
     handleDownloadReport,
     handleGenerateBundle,
