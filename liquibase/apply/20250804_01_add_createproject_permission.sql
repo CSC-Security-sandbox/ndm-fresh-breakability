0879014ac630fe18a2994497d811176a464a2f1a
AniketCalfus <aniket.darekar@calfus.com>
2025-08-04 22:11:18 +0530

    queries optimised

--- liquibase/apply/20250804_01_add_createproject_permission.sql
+++ liquibase/apply/20250804_01_add_createproject_permission.sql
@@ -2,7 +2,13 @@ INSERT INTO "permission" (
     id, permission_name, permission_status, created_at, created_by, updated_at, updated_by
 )
 SELECT
-    uuid_generate_v4(), 'CreateProject', 'active', now(), uuid_generate_v4(), now(), uuid_generate_v4()
+    uuid_generate_v4(), 
+    'CreateProject', 
+    'active', 
+    now(), 
+    (SELECT id FROM "user" WHERE email = 'admin@datamigrator.local'), 
+    now(), 
+    (SELECT id FROM "user" WHERE email = 'admin@datamigrator.local')
 WHERE NOT EXISTS (
     SELECT 1 FROM "permission" WHERE permission_name = 'CreateProject'
 );
@@ -15,9 +21,9 @@ SELECT
     r.id,
     p.id,
     now(),
-    uuid_generate_v4(),
+    (SELECT id FROM "user" WHERE email = 'admin@datamigrator.local'),
     now(),
-    uuid_generate_v4()
+    (SELECT id FROM "user" WHERE email = 'admin@datamigrator.local')
 FROM "role" r, "permission" p
 WHERE r.role_name = 'App Admin'
   AND p.permission_name = 'CreateProject'
--- liquibase/rollback/20250804_01_add_createproject_permission_rollback.sql
+++ liquibase/rollback/20250804_01_add_createproject_permission_rollback.sql
@@ -1,11 +1,12 @@
-DELETE FROM "role_permission"
-WHERE role_id = (
-    SELECT id FROM "role" WHERE role_name = 'App Admin'
+WITH permission_cte AS (
+    SELECT id AS permission_id FROM "permission" WHERE permission_name = 'CreateProject'
+),
+delete_role_permission AS (
+    DELETE FROM "role_permission"
+    WHERE role_id = (
+        SELECT id FROM "role" WHERE role_name = 'App Admin'
+    )
+    AND permission_id = (SELECT permission_id FROM permission_cte)
 )
-AND permission_id = (
-    SELECT id FROM "permission" WHERE permission_name = 'CreateProject'
-);
 DELETE FROM "permission"
-WHERE id = (
-    SELECT id FROM "permission" WHERE permission_name = 'CreateProject'
-);
+WHERE id = (SELECT permission_id FROM permission_cte);
