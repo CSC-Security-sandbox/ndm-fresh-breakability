e9e060f785b14d5671422bf56453fb6a3353fd15
omkesh sajjanwar <om13274@netapp.com>
2025-08-07 15:04:26 +0530

    Added SMB Support for Download error

--- ndm-api-tests/tests/download-error-report-e2e/DER_suite_test.go
+++ ndm-api-tests/tests/download-error-report-e2e/DER_suite_test.go
@@ -1,12 +1,21 @@
 package downloaderrorreporte2e
 
 import (
+	"flag"
 	. "ndm-api-tests/utils"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
+
+var ProtocolType, SourceVolumesArgs, DestinationVolumesArgs string
+
+func init() {
+	flag.StringVar(&ProtocolType, "protocol_type", "NFS", "Enter protocol_type (SMB / NFS)")
+	flag.StringVar(&SourceVolumesArgs, "src_volumes", "", "Enter comma separated source volumes")
+	flag.StringVar(&DestinationVolumesArgs, "dest_volumes", "", "Enter comma separated destination volumes")
+}
 
 func TestE2e(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -16,4 +25,5 @@ func TestE2e(t *testing.T) {
 var _ = BeforeSuite(func() {
 	LogDebug("Setting before the suite")
 	InitTestEnv()
+	UpdateConfVariables(ProtocolType, SourceVolumesArgs, DestinationVolumesArgs)
 })
