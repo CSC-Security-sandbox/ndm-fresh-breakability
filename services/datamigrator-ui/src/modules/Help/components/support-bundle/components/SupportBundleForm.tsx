45a24e325ac44fb0db4fe4d55391d41491a20836
Rutuja Deshmukh <rutuja.deshmukh@calfus.com>
2025-08-09 22:31:47 +0530

    Support Bundle Phase2

--- services/datamigrator-ui/src/modules/Help/components/support-bundle/components/SupportBundleForm.tsx
+++ services/datamigrator-ui/src/modules/Help/components/support-bundle/components/SupportBundleForm.tsx
@@ -1,13 +1,27 @@
-import { DatePicker, FormFieldSelect } from "@netapp/bxp-design-system-react";
+import {
+  DatePicker,
+  FormFieldSelect,
+  TreeSelect,
+} from "@netapp/bxp-design-system-react";
 import { Box } from "@components/container";
 import { useContext } from "react";
 import { SupportBundleContext } from "@modules/Help/components/support-bundle/context/context";
-import { METRICS_OPTIONS } from "@modules/Help/components/support-bundle/constants/support-bundle.constant";
+import {
+  METRICS_OPTIONS,
+  PROJECT_AND_WORKER_LABEL,
+} from "@modules/Help/components/support-bundle/constants/support-bundle.constant";
 import { SupportBundleFormErrorsType } from "@modules/Help/components/support-bundle/types/support-bundle.types";
+import "./SupportBundleForm.css";
 
 const SupportBundleForm = () => {
-  const { supportBundleForm, handleDateChange } =
-    useContext(SupportBundleContext);
+  const {
+    supportBundleForm,
+    handleDateChange,
+    treeSelectStyles,
+    handleSelectionChange,
+    wrapperClass,
+    projectWorkerData,
+  } = useContext(SupportBundleContext);
 
   const { startDate, endDate } = supportBundleForm?.formState;
   const formErrors =
@@ -15,8 +29,8 @@ const SupportBundleForm = () => {
   const datePickerError = formErrors?.startDate || formErrors?.endDate || "";
 
   return (
-    <Box className="flex flex-row gap-4 p-6 items-center">
-      <Box className="mb-5">
+    <Box className="flex flex-col gap-2 p-4">
+      <Box className="flex gap-4 items-center">
         <DatePicker
           label="Date Range"
           dateMode="range"
@@ -31,13 +45,38 @@ const SupportBundleForm = () => {
         />
       </Box>
 
-      <FormFieldSelect
-        label="Other Metrics"
-        name="otherMetrics"
-        form={supportBundleForm}
-        options={METRICS_OPTIONS}
-        isOptional
-      />
+      <Box className="flex flex-row gap-4 items-center">
+        <Box className="flex flex-col relative w-full mb-4">
+          <label className="text-sm mb-1 inline-block">
+            {PROJECT_AND_WORKER_LABEL}
+          </label>
+          <Box>
+            <style>{treeSelectStyles}</style>
+            <Box className={wrapperClass}>
+              <TreeSelect
+                name="projectWorker"
+                form={supportBundleForm}
+                treeData={projectWorkerData || []}
+                isSearchable
+                menuType="multi"
+                uniqueIdentifier="id"
+                onChange={handleSelectionChange}
+              />
+            </Box>
+          </Box>
+        </Box>
+
+        <FormFieldSelect
+          label="Other Metrics"
+          name="otherMetrics"
+          form={supportBundleForm}
+          options={METRICS_OPTIONS}
+          isOptional
+          isSearchable
+          isMulti
+          hasSelectAll
+        />
+      </Box>
     </Box>
   );
 };
