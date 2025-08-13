bee2d8cc9bb9bdef854a6089167d95e8de1ad2d1
RohitBhosale204 <Rohit.Bhosale@calfus.com>
2025-08-13 14:58:37 +0530

    standardized byte formatting from GB to GiB

--- services/datamigrator-ui/src/modules/jobs/discovery-preview/preview.decorators.ts
+++ services/datamigrator-ui/src/modules/jobs/discovery-preview/preview.decorators.ts
@@ -517,18 +517,22 @@ export function extractSystemFileStatAndDirectories(data: DataItemType[]) {
 }
 
 {
-  /* This function smartly convert bytes to KB, MB, GB, TB, PB, EB, ZB or YB */
+  /* This function smartly convert bytes to KiB, MiB, GiB, TiB, PiB, EiB, ZiB or YiB */
 }
-export function formatBytes(bytes: number, decimals = 2): string {
+
+export function formatBytes(bytes: number): string {
   if (bytes === 0) return "0 B";
 
-  const k = BYTES_IN_KILOBYTE;
+  const units = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+  const k = 1024;
   const i = Math.floor(Math.log(bytes) / Math.log(k));
+  const size = bytes / Math.pow(k, i);
 
-  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(decimals))} ${
-    BYTE_UNITS[i] || BYTE_UNITS[0]
-  }`;
+  return size === Math.floor(size)
+    ? `${size.toFixed(0)} ${units[i]}`
+    : `${size.toFixed(2)} ${units[i]}`;
 }
+
 
 {
   /* This function smartly convert numbers to K, M, B, T, Q, Quint, Sext, Sept */
