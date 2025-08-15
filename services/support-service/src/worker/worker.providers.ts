f3fdcb0517926e9848f76e88fe231812c6b99bbe
rutvikraj-calfus <rutvik.raj.champavat@calfus.com>
2025-08-15 11:26:46 +0530

    Generate system inventory log (#539)
    * Generating system inventory log
    
    * conflict resolved
    
    * Refactor timestamp handling to use formatUnixTimestamp utility in performance and system inventory processors

--- services/support-service/src/worker/worker.providers.ts
+++ services/support-service/src/worker/worker.providers.ts
@@ -30,6 +30,8 @@ export const workerProviders = [
           activitiesService.generatePerformanceMetricsCsv.bind(
             activitiesService,
           ),
+        generateSystemInventoryCsv:
+          activitiesService.generateSystemInventoryCsv.bind(activitiesService),
         generateConfigurationJobCsv:
           activitiesService.generateConfigurationJobCsv.bind(activitiesService),
       };
