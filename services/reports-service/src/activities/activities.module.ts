c49257a5ea81fe2c1a7ec057e3b9bd64ab0a2874
Kunal Avghade <kunal.avghade@calfus.com>
2025-08-19 14:48:31 +0530

    Docker puppeter change for pdf

--- services/reports-service/src/activities/activities.module.ts
+++ services/reports-service/src/activities/activities.module.ts
@@ -5,9 +5,14 @@ import { DiscoveryReportService } from './discovery-report/discovery-report.serv
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsEntity } from 'src/entities/reports.entity';
 import { JobRunEntity } from 'src/entities/jobrun.entity';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from 'src/config/database.config';
+import appConfig from 'src/config/app.config';
+import temporalConfig from 'src/config/temporal.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ load: [databaseConfig, appConfig, temporalConfig] }), 
     TypeOrmModule.forFeature([ReportsEntity, JobRunEntity]),
     GeneratorModule
   ],
