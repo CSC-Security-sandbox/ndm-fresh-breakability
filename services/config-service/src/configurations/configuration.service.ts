810e57d6fad1b2ab92ed413ed0b32267d4d096cb
Kunal Avghade <139209856+Kunal77avghade@users.noreply.github.com>
2025-10-03 11:38:06 +0000

    [NDM-2382] config server refresh on inactive status (fix) (#799)

--- services/config-service/src/configurations/configuration.service.ts
+++ services/config-service/src/configurations/configuration.service.ts
@@ -1367,19 +1367,19 @@ export class ConfigurationService {
         fetch all the job configurations that has any of the volumeIds in
         their sourcePathId or targetPathId and status is ACTIVE
       */
-      const jobConfigs = await this.jobConfigRepo
-        .createQueryBuilder('jobConfig')
-        .where(
-          'jobConfig.source_path_id IN (:...volumeIds) OR jobConfig.target_path_id IN (:...volumeIds)',
-          { volumeIds },
-        )
-        .andWhere('jobConfig.status = :status', { status: 'ACTIVE' })
-        .getMany();
-
+      const jobConfigs = await this.jobConfigRepo.find({
+          where: [{
+              status: JobStatus.Active,
+              sourcePathId: In(volumeIds),
+            }, {
+              status: JobStatus.Active,
+              targetPathId: In(volumeIds),
+          }]
+      })
       // check if any job config has schedule as SCHEDULING if yes then return false
       if (jobConfigs.some((jc) => jc.scheduler === 'SCHEDULING')) {
         this.logger.warn(
-          `Refresh is not possible for configuration ${configId} as there are jobs with SCHEDULING status`,
+          `Refresh is not possible for configuration ${configId} as there are jobs with SCHEDULING status jobs : ${JSON.stringify(jobConfigs.filter((jc) => jc.scheduler === 'SCHEDULING'))}`,
         );
         return false;
       }
@@ -1387,7 +1387,7 @@ export class ConfigurationService {
       // check if futureScheduleAt is not null for any job config, if yes then return false
       if (jobConfigs.some((jc) => !!jc.futureScheduleAt)) {
         this.logger.warn(
-          `Refresh is not possible for configuration ${configId} as there are jobs with futureScheduleAt set`,
+          `Refresh is not possible for configuration ${configId} as there are jobs with futureScheduleAt set: ${JSON.stringify(jobConfigs.filter((jc) => !!jc.futureScheduleAt))}`,
         );
         return false;
       }
