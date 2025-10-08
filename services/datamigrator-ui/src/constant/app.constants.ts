207bf5b34520dee09b52db14fddf3dfbe70cfd97
Jeevitha-dev <jv84182@netapp.com>
2025-10-08 11:11:40 +0530

    NDM-1543 : Updated default exclude path patterns

--- services/datamigrator-ui/src/constant/app.constants.ts
+++ services/datamigrator-ui/src/constant/app.constants.ts
@@ -4,7 +4,7 @@ import { format } from "date-fns";
 import StatusCellRenderer from "@/components/custom-cell-renderer/StatusCellRenderer";
 import NameCellRenderer from "@/components/custom-cell-renderer/NameCellRenderer";
 
-export const INITIAL_VALUE_EXCLUDE_PATH_PATTERN = `*/snapshot/*,*/logs/*,*/tmp/*`;
+export const INITIAL_VALUE_EXCLUDE_PATH_PATTERN = `*/~snapshot/*,*/.snapshot/*`;
 
 export const COL_DEF_FOR_USER = [
   {
