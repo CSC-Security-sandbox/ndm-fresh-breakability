b6be838c4864dc1c8cfd583e9aea9f31c7068043
Ashish Sinha <si82974@netapp.com>
2025-12-13 09:27:40 +0000

    add api for management server (#954)

--- services/config-service/src/config/database.config.ts
+++ services/config-service/src/config/database.config.ts
@@ -12,6 +12,7 @@ import { FileServerWorkingDirectoryMappingEntity } from 'src/entities/fileserver
 import { WorkerStatsEntity } from 'src/entities/worker-stats.entity';
 import { PathUploadsEntity } from 'src/entities/pathupload.entity';
 import { SupportBundleEntity } from 'src/entities/support-bundle-log.entity';
+import { ManagementServerEntity } from 'src/entities/ManagementServerEntity';
 
 export default registerAs(
   'typeorm',
@@ -39,6 +40,7 @@ export default registerAs(
       WorkerStatsEntity,
       PathUploadsEntity,
       SupportBundleEntity,
+      ManagementServerEntity,
     ],
     migrations: [],
   }),
