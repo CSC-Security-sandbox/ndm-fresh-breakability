8b8e15ec318ea0e6b8a3e3acfc22dbdad0935303
Ashish Sinha <si82974@netapp.com>
2025-12-16 09:45:43 +0000

    remove management_server and use config  table (#966)

---
+++ liquibase/apply/20251216_01_alter_config_and_file_server_table.sql
@@ -0,0 +1,26 @@
+ALTER TABLE datamigrator.config
+ADD COLUMN hostname varchar(255) NOT NULL;
+
+ALTER TABLE datamigrator.config
+ADD COLUMN port integer NULL;
+
+ALTER TABLE datamigrator.config
+ADD COLUMN server_type varchar NOT NULL;
+
+ALTER TABLE datamigrator.config
+ADD COLUMN username varchar(255);
+
+ALTER TABLE datamigrator.config
+ADD COLUMN password varchar(255);
+
+ALTER TABLE datamigrator.config
+ADD COLUMN tls_accepted boolean DEFAULT false;
+
+ALTER TABLE datamigrator.config
+ADD COLUMN tls_ca_certificate text;
+
+ALTER TABLE datamigrator.file_server
+ADD COLUMN file_server_name varchar NOT NULL;
+
+ALTER TABLE datamigrator.file_server
+ADD COLUMN status varchar(255);
