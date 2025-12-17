5b1476a95dd24552d3eddc9cbe8fc806e08038f9
Ashish Sinha <si82974@netapp.com>
2025-12-17 07:17:40 +0000

    add expiry and zoneid (#970)

--- liquibase/apply/20251216_01_alter_config_and_file_server_table.sql
+++ liquibase/apply/20251216_01_alter_config_and_file_server_table.sql
@@ -19,8 +19,14 @@ ADD COLUMN tls_accepted boolean DEFAULT false;
 ALTER TABLE datamigrator.config
 ADD COLUMN tls_ca_certificate text;
 
+ALTER TABLE datamigrator.config
+ADD COLUMN tls_expiry date NULL;
+
 ALTER TABLE datamigrator.file_server
 ADD COLUMN file_server_name varchar NOT NULL;
+
+ALTER TABLE datamigrator.file_server
+ADD COLUMN zone_id integer NULL;
 
 ALTER TABLE datamigrator.file_server
 ADD COLUMN status varchar(255);
